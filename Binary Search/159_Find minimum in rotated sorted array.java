e1b881f5efdc62e93bed526cd2dcc8902cc284c5
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-16 17:15:23 -0800

    Add files via upload

---
+++ Binary Search/159_Find minimum in rotated sorted array.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     * @param nums: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[nums.length - 1]){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (nums[start] < nums[nums.length - 1]){
+            return nums[start];
+        }
+        return nums[end];
+    }
+}
