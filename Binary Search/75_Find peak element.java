53c55fea51654d2928d1fff30b8b3515f6a46718
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-16 17:44:49 -0800

    Add files via upload

---
+++ Binary Search/75_Find peak element.java
@@ -0,0 +1,32 @@
+public class Solution {
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (nums[start] > nums[start + 1]){
+            return start;
+        }
+        
+        if (nums[end] > nums[end + 1]){
+            return end;
+        }
+        
+        return -1;
+    }
+}
