aeab3ea69db2b37de72fbf51a7a77017961bc5df
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-16 17:07:52 -0800

    Add files via upload

---
+++ Binary Search/460_Find k closest elements.java
@@ -0,0 +1,43 @@
+public class Solution {
+    /**
+     * @param A: an integer array
+     * @param target: An integer
+     * @param k: An integer
+     * @return: an integer array
+     */
+    public int[] kClosestNumbers(int[] nums, int target, int k) {
+        // write your code here
+        int[] res = new int[k];
+        if (nums == null || nums.length == 0){
+            return res;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        int count = 0;
+        while (start >= 0 && end < nums.length && count < k){
+            if (Math.abs(nums[start] - target) <= Math.abs(nums[end] - target)){
+                res[count++] = nums[start--];
+            } else{
+                res[count++] = nums[end++];
+            }
+        }
+        
+        while (start >= 0 && count < k){
+            res[count++] = nums[start--];
+        }
+        while (end < nums.length && count < k){
+            res[count++] = nums[end++];
+        }
+        
+        return res;
+    }
+}
