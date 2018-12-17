cd79288d7e69bea40ea62ee3a87aeac0319943cd
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-17 00:20:00 -0800

    Add files via upload

---
+++ BFS/127_Topological sorting.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     ArrayList<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+ * };
+ */
+
+public class Solution {
+    /*
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        // write your code here
+        ArrayList<DirectedGraphNode> res = new ArrayList<>();
+        Map<DirectedGraphNode, Integer> degrees = new HashMap<>();
+        
+        for (int i = 0; i < graph.size(); i++){
+            for (DirectedGraphNode neighbor: graph.get(i).neighbors){
+                degrees.put(neighbor, degrees.getOrDefault(neighbor, 0) + 1);
+            }
+        }
+        
+        Queue<DirectedGraphNode> queue = new LinkedList<>();
+        for (int i = 0; i < graph.size(); i++){
+            if (degrees.getOrDefault(graph.get(i), 0) == 0){
+                queue.offer(graph.get(i));
+            }
+        }
+        
+        int count = 0;
+        while (!queue.isEmpty()){
+            count++;
+            DirectedGraphNode curt = queue.poll();
+            res.add(curt);
+            for (DirectedGraphNode neighbor: curt.neighbors){
+                degrees.put(neighbor, degrees.get(neighbor) - 1);
+                if (degrees.get(neighbor) == 0){
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        
+        if (count != graph.size()){
+            return new ArrayList<>();
+        }
+        return res;
+    }
+}
