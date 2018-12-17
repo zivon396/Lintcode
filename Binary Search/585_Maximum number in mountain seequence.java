0aaed504399b267bd5379c0e742b6f327ce71794
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-16 17:06:07 -0800

    Add files via upload

---
+++ Binary Search/585_Maximum number in mountain seequence.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * @param nums: a mountain sequence which increase firstly and then decrease
+     * @return: then mountain top
+     */
+    public int mountainSequence(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        return Math.max(nums[start], nums[end]);
+    }
+}
