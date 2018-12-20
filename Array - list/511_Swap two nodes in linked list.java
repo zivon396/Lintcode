3331a7e64fa9a372fd804716919e57399d5a71e6
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-19 19:06:57 -0800

    Add files via upload

---
+++ Array - list/511_Swap two nodes in linked list.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: a ListNode
+     * @param v1: An integer
+     * @param v2: An integer
+     * @return: a new head of singly-linked list
+     */
+    public ListNode swapNodes(ListNode head, int v1, int v2) {
+        // write your code here
+        if (head == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode n1Pre = null, n2Pre = null;
+        ListNode curt = dummy;
+        
+        while (curt.next != null){
+            if (curt.next.val == v1){
+                n1Pre = curt;
+            }
+            if (curt.next.val == v2){
+                n2Pre = curt;
+            }
+            curt = curt.next;
+        }
+        
+        if (n1Pre == null || n2Pre == null){
+            return dummy.next;
+        }
+        
+        ListNode n1Node = n1Pre.next;
+        ListNode n1Post = n1Node.next;
+        ListNode n2Node = n2Pre.next;
+        ListNode n2Post = n2Node.next;
+        
+        if (n1Post == n2Node){
+            n1Pre.next = n2Node;
+            n2Node.next = n1Node;
+            n1Node.next = n2Post;
+        } else if (n2Post == n1Node){
+            n2Pre.next = n1Node;
+            n1Node.next = n2Node;
+            n2Node.next = n1Post;
+        } else {
+            n1Pre.next = n2Node;
+            n2Node.next = n1Node.next;
+            n2Pre.next = n1Node;
+            n1Node.next = n2Post;
+        }
+        
+        
+        return dummy.next;
+    }
+}
