11f32050e88876b42448015a2acb3157fbfadefd
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-19 18:34:54 -0800

    Add files via upload

---
+++ Array - list/170_Rotate list.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: the List
+     * @param k: rotate to the right k places
+     * @return: the list after rotation
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        // write your code here
+        if (head == null || k <= 0){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        int size = 0;
+        while (head.next != null){
+            size++;
+            head = head.next;
+        }
+        ListNode nlast = head;
+        
+        k = k % size;
+        if (k == 0){
+            return dummy.next;
+        }
+        k = size - k;
+        
+        head = dummy;
+        for (int i = 0; i < k; i++){
+            head = head.next;
+        }
+        
+        ListNode newHead = head.next;
+        head.next = null;
+        nlast.next = dummy.next;
+        
+        return newHead;
+    }
+}
