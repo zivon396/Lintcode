211f4c327f73b8704b3ecdc3c5b9ad81a79619da
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-19 16:49:21 -0800

    Add files via upload

---
+++ Array - list/35_Reverse linked list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: n
+     * @return: The new head of reversed linked list.
+     */
+    public ListNode reverse(ListNode head) {
+        // write your code here
+        ListNode pre = null;
+        
+        while (head != null){
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        
+        return pre;
+    }
+}
