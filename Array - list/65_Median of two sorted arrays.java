9e7b0df934549c982e4bbc78e512bf55c49ab59e
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-20 01:01:32 -0800

    Update 65_Median of two sorted arrays.java

--- Array - list/65_Median of two sorted arrays.java
+++ Array - list/65_Median of two sorted arrays.java
@@ -1,3 +1,70 @@
+//version 1: binary sort exception
+public class Solution {
+    /*
+     * @param A: An integer array
+     * @param B: An integer array
+     * @return: a double whose format is *.5 or *.0
+     */
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        // write your code here
+        int size = A.length + B.length;
+        
+        if (size % 2 == 1){
+            return 1.0 * findKth(A, B, size / 2 + 1);
+        }
+        
+        return findKth(A, B, size / 2) / 2.0 + findKth(A, B, size / 2 + 1) / 2.0;
+    }
+    
+    private int findKth (int[] A, int[] B, int k){
+        if (A.length == 0){
+            return B[k - 1];
+        }
+        if (B.length == 0){
+            return A[k - 1];
+        }
+        
+        int start = Math.min(A[0], B[0]);
+        int end = Math.max(A[A.length - 1], B[B.length - 1]);
+        
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (get_leq(A, mid) + get_leq(B, mid) >= k){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (get_leq(A, start) + get_leq(B, start) == k){
+            return start;
+        }
+        
+        return end;
+    }
+    
+    private int get_leq (int[] nums, int target){
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target){
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (nums[end] <= target){
+            return end + 1;
+        }
+        if (nums[start] <= target){
+            return start + 1;
+        }
+        return 0;
+    }
+}
+
+//version 2: divide & conquer(每次移走1/2)
 public class Solution {
     /*
      * @param A: An integer array
