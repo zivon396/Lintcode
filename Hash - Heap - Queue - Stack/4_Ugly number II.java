4dd3e9287d45e8c346957d41a22b0e479667252d
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-19 16:21:01 -0800

    Add files via upload

---
+++ Hash - Heap - Queue - Stack/4_Ugly number II.java
@@ -0,0 +1,35 @@
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: the nth prime number as description.
+     */
+    private long res;
+    
+    public int nthUglyNumber(int n) {
+        // write your code here
+        if (n <= 6){
+            return n;
+        }
+        
+        long[] primes = new long[] {2, 3, 5};
+        Queue<Long> heap = new PriorityQueue<Long>();
+        Set<Long> set = new HashSet<>();
+        for (int i = 0; i < 3; i++){
+            heap.add(primes[i]);
+            set.add(primes[i]);
+        }
+        
+        for (int j = 0; j < n - 1; j++){
+            res = heap.poll();
+            for (int i = 0; i < 3; i++){
+                long next = primes[i] * res;
+                if (!set.contains(next)){
+                    heap.add(next);
+                    set.add(next);
+                }
+            }
+        }
+        
+        return (int)res;
+    }
+}
