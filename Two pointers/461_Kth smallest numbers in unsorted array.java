3ab4a25865d4ec3380d31510949d1673b2755426
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-20 22:45:47 -0800

    Add files via upload

---
+++ Two pointers/461_Kth smallest numbers in unsorted array.java
@@ -0,0 +1,51 @@
+public class Solution {
+    /**
+     * @param k: An integer
+     * @param nums: An integer array
+     * @return: kth smallest element
+     */
+    public int kthSmallest(int k, int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0 || k < 1 || k > nums.length){
+            return -1;
+        }
+        return partition(nums, 0, nums.length - 1, k - 1);
+    }
+    
+    private int partition(int[] nums, int start, int end, int k) {
+        if (start >= end) {
+            return nums[k];
+        }
+        
+        int left = start, right = end;
+        int pivot = nums[(start + end) / 2];
+        
+        while (left <= right) {
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+        
+        if (k <= right) {
+            return partition(nums, start, right, k);
+        }
+        if (k >= left) {
+            return partition(nums, left, end, k);
+        }
+        return nums[k];
+    }    
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
