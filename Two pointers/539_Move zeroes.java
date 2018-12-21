60290d75e4367ae7292a68f07d006010cf5dfcba
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-20 16:15:37 -0800

    Add files via upload

---
+++ Two pointers/539_Move zeroes.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /**
+     * @param nums: an integer array
+     * @return: nothing
+     */
+    public void moveZeroes(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0){
+            return;
+        }
+        
+        int left = 0, right = 0;
+        while (right < nums.length){
+            if (nums[right] != 0){
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+                left++;
+            }
+            right++;
+        }
+        
+    }
+}
