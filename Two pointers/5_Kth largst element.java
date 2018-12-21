301d857e6c623600948dd28ae728e0f37ca7657d
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-21 04:00:17 -0800

    Add files via upload

---
+++ Two pointers/5_Kth largst element.java
@@ -0,0 +1,51 @@
+public class Solution {
+    /**
+     * @param n: An integer
+     * @param nums: An array
+     * @return: the Kth largest element
+     */
+    public int kthLargestElement(int k, int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        
+        int res = partition(nums, 0, nums.length - 1, nums.length - k);
+        
+        return res;
+    }
+    
+    private int partition (int[] nums, int start, int end, int k){
+        if (start >= end){
+            return nums[k];
+        }
+        
+        int left = start, right = end;
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
+        while (left <= right){
+            while (left <= right && nums[left] < pivot){
+                left++;
+            }
+            while (left <= right && nums[right] > pivot){
+                right--;
+            }
+            
+            if (left <= right){
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        
+        if (k <= right){
+            return partition(nums, start, right, k);
+        }
+        if (k >= left){
+            return partition(nums, left, end, k);
+        }
+        return nums[k];
+    }
+}
