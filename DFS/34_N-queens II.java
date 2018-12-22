b979c19bad41c1d94f7b78bcc585110f2c9038f1
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-21 20:00:49 -0800

    Add files via upload

---
+++ DFS/34_N-queens II.java
@@ -0,0 +1,50 @@
+public class Solution {
+    /**
+     * @param n: The number of queens.
+     * @return: The total number of distinct solutions.
+     */
+     
+    private int count = 0;
+    
+    public int totalNQueens(int n) {
+        // write your code here
+        List<Integer> per = new ArrayList<>();
+        
+        helper(n, per);
+        
+        return count;
+    }
+    
+    private void helper (int n, List<Integer> per){
+        if (per.size() == n){
+            count++;
+            return;
+        }
+        
+        for (int i = 1; i <= n; i++){
+            if (!isValid(per, i)){
+                continue;
+            }
+            
+            per.add(i);
+            helper(n, per);
+            per.remove(per.size() - 1);
+        }
+    }
+    
+    private boolean isValid (List<Integer> per, int i){
+        int row = per.size();
+        for (int rowIndex = 0; rowIndex < row; rowIndex++){
+            if (i == per.get(rowIndex)){
+                return false;
+            }
+            if (per.get(rowIndex) + rowIndex == i + row){
+                return false;
+            }
+            if (per.get(rowIndex) - rowIndex == i - row){
+                return false;
+            }
+        }
+        return true;
+    }
+}
