5af9d6bd7d2439e74b8f649c5ca1590081a3186d
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-21 23:45:23 -0800

    Add files via upload

---
+++ DFS/426_Restore IP address.java
@@ -0,0 +1,53 @@
+public class Solution {
+    /**
+     * @param s: the IP string
+     * @return: All possible valid IP addresses
+     */
+    public List<String> restoreIpAddresses(String s) {
+        // write your code here
+        List<String> res = new ArrayList<>();
+        if (s == null || s.length() == 0){
+            return res;
+        }
+        
+        
+        List<String> com = new ArrayList<>();
+        helper(s, com, 0, res);
+        
+        return res;
+    }
+    
+    private void helper (String s, List<String> com, int startIndex, List<String> res){
+        if (startIndex >= s.length()){
+            if (com.size() != 4){
+                return;
+            }
+            String rst = "";
+            for (String str: com){
+                rst += str;
+                rst += ".";
+            }
+            rst = rst.substring(0, rst.length() - 1);
+            res.add(new String(rst));
+            return;
+        }
+        
+        if (s.charAt(startIndex) == '0'){
+            com.add("0");
+            helper(s, com, startIndex + 1, res);
+            com.remove(com.size() - 1);
+        }
+        
+        else{
+            for (int i = 0; i < 3 && startIndex + i < s.length(); i++){
+                String str = s.substring(startIndex, startIndex + i + 1);
+                int num = Integer.parseInt(str);
+                if (num < 256){
+                    com.add(str);;
+                    helper(s, com, startIndex + i + 1, res);
+                    com.remove(com.size() - 1);
+                }
+            }
+        }
+    }
+}
