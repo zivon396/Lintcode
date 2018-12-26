93b6f0c9374c8e6a708310100127bc40eeae065e
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-25 16:45:24 -0800

    Add files via upload

---
+++ Binary tree/900_Closest binary seach tree value.java
@@ -0,0 +1,47 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the given BST
+     * @param target: the given target
+     * @return: the value in the BST that is closest to the target
+     */
+    public int closestValue(TreeNode root, double target) {
+        // write your code here
+        return helper(root, target);
+    }
+    
+    private int helper (TreeNode root, double target){
+        if (root == null){
+            return Integer.MAX_VALUE;
+        }
+        
+        if (root.val > target){
+            if (root.left != null){
+                int left = helper(root.left, target);
+                if (Math.abs(left - target) < Math.abs(root.val - target)){
+                    return left;
+                }
+            }
+        } else {
+            if (root.right != null){
+                int right = helper(root.right, target);
+                if (Math.abs(right - target) < Math.abs(root.val - target)){
+                    return right;
+                }
+            }
+        }
+        
+        return root.val;
+    }
+}
