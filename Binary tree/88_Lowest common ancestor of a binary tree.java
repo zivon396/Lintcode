add1b022df8a8b4459ebb1d14b3afbfe0e32e837
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-25 18:36:28 -0800

    Add files via upload

---
+++ Binary tree/88_Lowest common ancestor of a binary tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of the binary search tree.
+     * @param A: A TreeNode in a Binary.
+     * @param B: A TreeNode in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
+        // write your code here
+        TreeNode res = helper(root, A, B);
+        
+        return res;
+    }
+    
+    private TreeNode helper (TreeNode root, TreeNode A, TreeNode B){
+        if (root == null){
+            return null;
+        }
+        if (root == A || root == B){
+            return root;
+        }
+        
+        TreeNode left = helper(root.left, A, B);
+        TreeNode right = helper(root.right, A, B);
+        
+        if (left != null && right != null){
+            return root;
+        }
+        
+        if (left != null){
+            return left;
+        }
+        if (right != null){
+            return right;
+        }
+        
+        return null;
+    }
+}
