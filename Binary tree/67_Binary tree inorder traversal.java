29e7734e3feee99b3f33d65acfb3903676a1fc47
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-25 19:15:18 -0800

    Add files via upload

---
+++ Binary tree/67_Binary tree inorder traversal.java
@@ -0,0 +1,64 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+// version 1: traverse
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        List<Integer> res = new ArrayList<Integer>();
+        
+        helper(root, res);
+        
+        return res;
+    }
+    
+    private void helper(TreeNode root, List<Integer> res){
+        if (root == null){
+            return;
+        }
+        
+        helper(root.left, res);
+        res.add(root.val);
+        helper(root.right, res);
+    }
+}
+
+//version 2: stack
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<>();
+        
+        TreeNode curt = root;
+        
+        while (curt != null || !stack.isEmpty()){
+            while (curt != null){
+                stack.push(curt);
+                curt = curt.left;
+            }
+            
+            curt = stack.pop();
+            res.add(curt.val);
+            curt = curt.right;
+        }
+        
+        return res;
+    }
+}
