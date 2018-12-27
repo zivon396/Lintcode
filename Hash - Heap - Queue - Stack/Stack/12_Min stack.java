c5ee1786b8556eebd537d723dd7a0ea587d8e5e6
zivon396 <42732539+zivon396@users.noreply.github.com>
2018-12-26 17:28:37 -0800

    Add files via upload

---
+++ Hash - Heap - Queue - Stack/Stack/12_Min stack.java
@@ -0,0 +1,40 @@
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> min_stack;
+    public MinStack() {
+        // do intialization if necessary
+        stack = new Stack<Integer>();
+        min_stack = new Stack<Integer>();
+    }
+
+    /*
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void push(int number) {
+        // write your code here
+        stack.push(number);
+        if (min_stack.isEmpty()){
+            min_stack.push(number);
+        } else {
+            min_stack.push(Math.min(number, min_stack.peek()));
+        }
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int pop() {
+        // write your code here
+        min_stack.pop();
+        return stack.pop();
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int min() {
+        // write your code here
+        return min_stack.peek();
+    }
+}
