8e038c731aa5b6f1f44112e7385a52d56a0e8a86
zivon396 <42732539+zivon396@users.noreply.github.com>
2019-01-01 02:35:54 -0800

    Add files via upload

---
+++ Binary Search/617_Maximum average subarray.java
@@ -0,0 +1,50 @@
+public class Solution {
+    /**
+     * @param nums: an array with positive and negative numbers
+     * @param k: an integer
+     * @return: the maximum average
+     */
+    private boolean canFind(int[] A, int K, double avg) {
+        int i, k;
+        double rightSum = 0, leftSum = 0, minLeftSum = 0;
+        for (i = 0; i < K; ++i) {
+            rightSum += A[i] - avg;
+        }
+        
+        for (i = K; i <= A.length; ++i) {
+            if (rightSum - minLeftSum >= 0) {
+                return true;
+            }
+            
+            if (i < A.length) {
+                rightSum += A[i] - avg;
+                leftSum += A[i - K] - avg;
+                minLeftSum = Math.min(minLeftSum, leftSum);
+            }
+        }
+        
+        return false;
+    } 
+     
+    public double maxAverage(int[] A, int K) {
+        int i;
+        double start, stop, mid;
+        start = stop = A[0];
+        for (i = 0; i < A.length; ++i) {
+            start = Math.min(A[i], start);
+            stop = Math.max(A[i], stop);
+        }
+        
+        while (start + 1e-5 < stop) {
+            mid = (start + stop) / 2;
+            if (canFind(A, K, mid)) {
+                start = mid;
+            }
+            else {
+                stop = mid;
+            }
+        }
+        
+        return start;
+    }
+}
