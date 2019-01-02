ab9890b72c8b445eb9287dd9c2468eab635f9ad3
zivon396 <42732539+zivon396@users.noreply.github.com>
2019-01-02 01:32:49 -0800

    Update 126_Max tree.java

--- Hash - Heap - Queue - Stack/Stack/126_Max tree.java
+++ Hash - Heap - Queue - Stack/Stack/126_Max tree.java
@@ -9,7 +9,7 @@
  *     }
  * }
  */
-
+// version 1: for + while
 public class Solution {
     /**
      * @param A: Given an integer array with no duplicates.
@@ -38,6 +38,41 @@ public class Solution {
             stack.push(node);
         }
         
+        return stack.peek().left;
+    }
+}
+
+//version 2: 无 while loop 版
+public class Solution {
+    /**
+     * @param A: Given an integer array with no duplicates.
+     * @return: The root of max tree.
+     */
+    public TreeNode maxTree(int[] A) {
+        // write your code here
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = new TreeNode(A[0]);
+        
+        for (int i = 0; i <= A.length; i++){
+            if (stack.isEmpty() || node.val <= stack.peek().val){
+                stack.push(node);
+                node = i >= A.length - 1 ? new TreeNode(Integer.MAX_VALUE) : new TreeNode(A[i+1]);
+            } else {
+                TreeNode son = stack.pop();
+                if (stack.isEmpty()){
+                    node.left = son;
+                    i--;
+                    continue;
+                }
+                TreeNode left = stack.peek();
+                if (left.val < node.val){
+                    left.right = son;
+                } else {
+                    node.left = son;
+                }
+                i--;
+            }
+        }
         return stack.peek().left;
     }
 }
