740967c91f2007c430e0a6c047709116b0d9a854
zivon396 <42732539+zivon396@users.noreply.github.com>
2019-01-02 20:20:39 -0800

    Add files via upload

---
+++ Hash - Heap - Queue - Stack/Heap/543_Kth largest in N arrays.java
@@ -0,0 +1,47 @@
+class Element {
+    public int x, y;
+    public int val;
+    public Element (int x, int y, int val){
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
+}
+
+public class Solution {
+    /**
+     * @param arrays: a list of array
+     * @param k: An integer
+     * @return: an integer, K-th largest element in N arrays
+     */
+    private Comparator<Element> elemCom = new Comparator<Element>(){
+        public int compare (Element left, Element right){
+            return right.val - left.val;
+        }
+    };
+    
+    public int KthInArrays(int[][] arrays, int k) {
+        // write your code here
+        Queue<Element> heap = new PriorityQueue<Element>(k, elemCom);
+        
+        for (int i = 0; i < arrays.length; i++){
+            if (arrays[i] == null || arrays[i].length == 0){
+                continue;
+            }
+            int len = arrays[i].length;
+            Arrays.sort(arrays[i]);
+            heap.offer(new Element(i, len - 1, arrays[i][len - 1]));
+        }
+        
+        for (int i = 0; i < k - 1; i++){
+            Element curt = heap.poll();
+            if (curt.y - 1 >= 0){
+                curt.y--;
+                curt.val = arrays[curt.x][curt.y];
+                heap.add(curt);
+            }
+        }
+        
+        return heap.peek().val;
+    }
+}
