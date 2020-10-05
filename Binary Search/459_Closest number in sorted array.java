b047533a97338b7f0df4c2f088ae8a1dac4db81d
zivon396 <42732539+zivon396@users.noreply.github.com>
2020-10-04 18:22:15 -0700

    Update 459_Closest number in sorted array.java

--- Binary Search/459_Closest number in sorted array.java
+++ Binary Search/459_Closest number in sorted array.java
@@ -13,16 +13,14 @@ public class Solution {
         int start = 0 , end = nums.length - 1;
         while (start + 1 < end){
             int mid = start + (end - start) / 2;
-            if (nums[mid] == target){
-                return mid;
-            } else if (nums[mid] < target){
+            if (nums[mid] < target){
                 start = mid;
             } else {
                 end = mid;
             }
         }
         
-        if (Math.abs(nums[start] - target) < Math.abs(nums[end] - target)){
+        if (target - nums[start] < nums[end] - target){
             return start;
         } else {
             return end;
