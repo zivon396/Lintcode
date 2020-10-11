d75c0b5e3ba5b377125e1cd5154bdf2d1ad9b197
zivon396 <42732539+zivon396@users.noreply.github.com>
2020-10-11 03:20:18 -0700

    Update 62_Search in rotated sorted array.java

--- Binary Search/62_Search in rotated sorted array.java
+++ Binary Search/62_Search in rotated sorted array.java
@@ -1,3 +1,4 @@
+// 方法 1: 以 nums[mid] 和 nums[nums.length - 1] 为双重比较标准
 public class Solution {
     /**
      * @param A: an integer rotated sorted array
@@ -24,6 +25,48 @@ public class Solution {
                     end = mid;
                 } else {
                     start = mid;
+                }
+            }
+        }
+        
+        if (nums[start] == target){
+            return start;
+        }
+        if (nums[end] == target){
+            return end;
+        }
+        
+        return -1;
+    }
+}
+
+// 方法 2: 以 target 和 nums[nums.length - 1] 为双重比较标准
+public class Solution {
+    /**
+     * @param A: an integer rotated sorted array
+     * @param target: an integer to be searched
+     * @return: an integer
+     */
+    public int search(int[] nums, int target) {
+        // write your code here
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (target <= nums[nums.length - 1]){
+                if (nums[mid] > target && nums[mid] <= nums[nums.length - 1]){
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                if (nums[mid] > nums[nums.length - 1] && nums[mid] < target){
+                    start = mid;
+                } else {
+                    end = mid;
                 }
             }
         }
