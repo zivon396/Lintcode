9a0bfd860c43d25b25f7438292ce8c961cd9691c
zivon396 <42732539+zivon396@users.noreply.github.com>
2020-10-11 02:48:23 -0700

    Update 414_Divide two integers.java

--- Binary Search/414_Divide two integers.java
+++ Binary Search/414_Divide two integers.java
@@ -4,7 +4,7 @@ public class Solution {
      * @param divisor: the divisor
      * @return: the result
      */
-    // 本质上和 Pow(x, n) 是一样的, 都是把 n 分解为 2的幂的和
+    // 本质上和 Pow(x, n) 是一样的, 都是把 n 分解为 2的幂的和. 不同之处在于 Pow(x, n)是从低到高(次幂)来找, 而本题是从高到低来找.
     // Integer.MIN_VALUE 在取绝对值的时候会溢出，所以要用 long
     // 时间复杂度应该是 (log(dividend/divisor))²
     public int divide(int dividend, int divisor) {
