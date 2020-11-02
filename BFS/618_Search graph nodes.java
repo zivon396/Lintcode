b3061c3f8d59bacf893d60d9dd0a24321d26b680
zivon396 <42732539+zivon396@users.noreply.github.com>
2020-11-01 18:39:37 -0800

    Update 618_Search graph nodes.java

--- BFS/618_Search graph nodes.java
+++ BFS/618_Search graph nodes.java
@@ -10,85 +10,37 @@
  * };
  */
 
-// version 1: 用set存储degree，不用再加一层for循环
 public class Solution {
-    /*
-     * @param graph: a list of Undirected graph node
-     * @param s: Undirected graph node
-     * @param t: Undirected graph nodes
-     * @return: an integer
+    /**
+     * @param graph a list of Undirected graph node
+     * @param values a hash mapping, <UndirectedGraphNode, (int)value>
+     * @param node an Undirected graph node
+     * @param target an integer
+     * @return the a node
      */
-    public int sixDegrees(List<UndirectedGraphNode> graph, UndirectedGraphNode s, UndirectedGraphNode t) {
-        // write your code here
-        Queue<UndirectedGraphNode> queue = new LinkedList<>();
-        Map<UndirectedGraphNode, Integer> visited = new HashMap<>();
-        
-        if (s == t){
-            return 0;
-        }
-        
-        queue.offer(s);
-        visited.put(s, 0);
-        
-        while (!queue.isEmpty()){
-            UndirectedGraphNode node = queue.poll();
-            int count = visited.get(node);
-            for (UndirectedGraphNode neighbor: node.neighbors){
-                if (neighbor == t){
-                    return count + 1;
-                }
-                if (!visited.containsKey(neighbor)){
-                    visited.put(neighbor, count + 1);
-                    queue.offer(neighbor);
+    public UndirectedGraphNode searchNode(ArrayList<UndirectedGraphNode> graph,
+                                          Map<UndirectedGraphNode, Integer> values,
+                                          UndirectedGraphNode node,
+                                          int target) {
+        // Write your code here
+        Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+        Set<UndirectedGraphNode> hash = new HashSet<UndirectedGraphNode>();
+
+        queue.offer(node);
+        hash.add(node);
+
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode head = queue.poll();
+            if (values.get(head) == target) {
+                return head;
+            }
+            for (UndirectedGraphNode nei : head.neighbors) {
+                if (!hash.contains(nei)){
+                    queue.offer(nei);
+                    hash.add(nei);
                 }
             }
         }
-        
-        return -1;
-    }
-}
-
-
-//version 2:
-public class Solution {
-    /*
-     * @param graph: a list of Undirected graph node
-     * @param s: Undirected graph node
-     * @param t: Undirected graph nodes
-     * @return: an integer
-     */
-    public int sixDegrees(List<UndirectedGraphNode> graph, UndirectedGraphNode s, UndirectedGraphNode t) {
-        // write your code here
-        if (graph == null || graph.size() == 0){
-            return -1;
-        }
-        if (s == t){
-            return 0;
-        }
-        
-        Queue<UndirectedGraphNode> queue = new LinkedList<>();
-        Set<UndirectedGraphNode> set = new HashSet<>();
-        set.add(s);
-        queue.offer(s);
-        
-        int count = 0;
-        while (!queue.isEmpty()){
-            int size = queue.size();
-            count++;
-            for (int i = 0; i < size; i++){
-                UndirectedGraphNode curt = queue.poll();
-                for (UndirectedGraphNode neighbor: curt.neighbors){
-                    if (neighbor == t){
-                        return count;
-                    }
-                    if (!set.contains(neighbor)){
-                        set.add(neighbor);
-                        queue.offer(neighbor);
-                    }
-                }
-            }
-        }
-        
-        return -1;
+        return null;
     }
 }
