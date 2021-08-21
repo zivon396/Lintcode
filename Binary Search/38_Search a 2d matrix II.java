7b3a4e9d4ada428e9e6ec9802c256e28b8b7e950
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-08-21 02:40:11 -0700

    Update 38_Search a 2d matrix II.java

--- Binary Search/38_Search a 2d matrix II.java
+++ Binary Search/38_Search a 2d matrix II.java
@@ -4,6 +4,7 @@ public class Solution {
      * @param: A number you want to search in the matrix
      * @return: An integer indicate the occurrence of target in the given matrix
      */
+    // 如果 matrix[i][j] == target, 则往右上移一格.
     public int searchMatrix(int[][] matrix, int target) {
         // check corner case
         if (matrix == null || matrix.length == 0) {
