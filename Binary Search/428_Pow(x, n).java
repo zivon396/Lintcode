c75c2d4d9291f51ea4f2fd9187c6f440f3336cfd
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-08-23 23:11:36 -0700

    Update 428_Pow(x, n).java

--- Binary Search/428_Pow(x, n).java
+++ Binary Search/428_Pow(x, n).java
@@ -1,3 +1,4 @@
+// 方法 1: m 右移
 public class Solution {
     /**
      * @param x: the base number
@@ -26,6 +27,41 @@ public class Solution {
             }
             temp *= temp;
             m >>= 1;
+        }
+        
+        return res;
+    }
+}
+
+// 方法 2: tail 左移 - 注意此时 tail 需要是 long, 不然会越界.
+public class Solution {
+    /**
+     * @param x: the base number
+     * @param n: the power number
+     * @return: the result
+     */
+    public double myPow(double x, int n) {
+        // write your code here
+        if (x == 0 || x == 1 || n == 1){
+            return x;
+        }
+        
+        long m = n;
+        if (n < 0){
+            m = -m;
+            x = 1.0 / x;
+        }
+        
+        long tail = 1;
+        double res = 1;
+        double temp = x;
+        
+        while (tail <= m){
+            if ((tail & m) != 0){
+                res *= temp;
+            }
+            temp *= temp;
+            tail <<= 1;
         }
         
         return res;
