231dcaf42e9c9d38c12b0763eabd977967a0a0ef
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-09-19 23:28:57 -0700

    Update 427_Generate parentheses.java

--- DFS/427_Generate parentheses.java
+++ DFS/427_Generate parentheses.java
@@ -1,3 +1,4 @@
+// left 代表剩余还能被加的 "(", value 代表已经加过的 "(".
 public class Solution {
     /**
      * @param n: n pairs
@@ -28,6 +29,50 @@ public class Solution {
         }
         
         if (value > 0){
+            com += ")";
+            helper(n, com, left, value - 1, res);
+            com = com.substring(0, com.length() - 1);
+        }
+    }
+}
+
+// 方法二: left 代表已经加过的 "(", value 代表剩余还需完成的 "()".
+public class Solution {
+    /**
+     * @param n: n pairs
+     * @return: All combinations of well-formed parentheses
+     */
+    public List<String> generateParenthesis(int n) {
+        // write your code here
+        List<String> res = new ArrayList<>();
+        if (n == 0){
+            return res;
+        }
+
+        String com = "";
+        int left = 0, value = n;
+        helper(n, com, left, value, res);
+
+        return res;
+    }
+
+    private void helper (int n,
+                         String com,
+                         int left,
+                         int value,
+                         List<String> res){
+        if (value == 0 && left == n){
+            res.add(new String(com));
+            return;
+        }
+
+        if (left < n){
+            com += "(";
+            helper(n, com, left + 1, value, res);
+            com = com.substring(0, com.length() - 1);
+        }
+
+        if (value > 0 && left + value > n){
             com += ")";
             helper(n, com, left, value - 1, res);
             com = com.substring(0, com.length() - 1);
