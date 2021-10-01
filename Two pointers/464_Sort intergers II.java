2cba3c0ae34cefdc37296d71eb4567c7c2e85e3f
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-10-01 01:18:38 -0700

    Update 464_Sort intergers II.java

--- Two pointers/464_Sort intergers II.java
+++ Two pointers/464_Sort intergers II.java
@@ -1,3 +1,85 @@
+// Quick sort
+    public void quickSort(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+    }
+    
+    private void quickSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        
+        int left = start;
+        int right = end;
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
+        
+        // overview sorted
+        while (left <= right) {
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+            
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+            
+            // swap left >= p with right <= p to make left <= p right >= p
+            if (left <= right) {
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        
+        quickSort(nums, start, right);
+        quickSort(nums, left, end);
+    }
+
+// Merge sort
+    public void mergeSort(int[] nums) {
+        int[] tmp = new int[nums.length];
+        mergeSort(nums, tmp, 0, nums.length - 1);
+    }
+    
+    private void mergeSort(int[] nums, int[] tmp, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        
+        // divide
+        int mid = start + (end - start) / 2;
+        mergeSort(nums, tmp, start, mid);
+        mergeSort(nums, tmp, mid + 1, end);
+        
+        // merge
+        int index = start;
+        int leftStart = start;
+        int rightStart = mid + 1;
+        
+        while (leftStart <= mid && rightStart <= end) {
+            if (nums[leftStart] <= nums[rightStart]) {
+                tmp[index++] = nums[leftStart++];
+            } else {
+                tmp[index++] = nums[rightStart++];
+            }
+        }
+        
+        while (leftStart <= mid) {
+            tmp[index++] = nums[leftStart++];
+        }
+        
+        while (rightStart <= end) {
+            tmp[index++] = nums[rightStart++];
+        }
+        
+        for (int i = start; i <= end; i++) {
+            nums[i] = tmp[i];
+        }
+    }
+
+// Heap sort
 public class Solution {
     /**
      * @param A: an integer array
