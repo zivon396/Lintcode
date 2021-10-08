90302663f8693da4d4d56e8e34cb3d0c55654878
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-10-08 01:31:03 -0700

    Create 128_Hash function.java

---
+++ Hash - Heap - Queue - Stack/128_Hash function.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int hashCode(char[] key,int HASH_SIZE) {
+        long ans = 0;
+        for(int i = 0; i < key.length;i++) {
+            ans = (ans * 33 + (int)(key[i])) % HASH_SIZE; 
+        }
+	return (int)ans;
+    }
+};
