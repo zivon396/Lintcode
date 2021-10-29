41a4046848f49cc3f4ef0b6ed0b0af60758374d1
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-10-29 00:14:58 -0700

    Create 513_Perfect squares.java

---
+++ DP/513_Perfect squares.java
@@ -0,0 +1,25 @@
+// DP
+// 时间复杂度 O(n^(3/2))
+// 空间复杂度 O(n)
+public class Solution {
+    /**
+     * @param n a positive integer
+     * @return an integer
+     */
+    public int numSquares(int n) {
+        // Write your code here
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for (int i = 0; i * i <= n; ++i) {
+            dp[i * i] = 1;
+        }
+
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
