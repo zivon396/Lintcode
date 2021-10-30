1b72052166fb51500449583b05cbd6c8d29f289e
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-10-30 02:33:13 -0700

    Update 1090_Map sum pairs.java

--- Trie - UnionFind/Trie/1090_Map sum pairs.java
+++ Trie - UnionFind/Trie/1090_Map sum pairs.java
@@ -1,70 +1,56 @@
-class TrieNode {
-    private TrieNode[] children;
-    public int value;
-    
-    public TrieNode (){
-        children = new TrieNode[26];
-        value = 0;
+// 注意 insert 的时候每个途经的 node 都要 + sum
+// old key 有新 value 的时候, insert (val - old)
+class MapSum {
+
+    public class TrieNode {
+        public TrieNode[] son = new TrieNode[26];
+        public int sum = 0;
     }
-    
-    public void insert (String word, int value, int index){
-        if (index == word.length()){
-            this.value = value;
-            return;
-        }
+
+    public class Trie {
+        public TrieNode root = new TrieNode();
         
-        int pos = word.charAt(index) - 'a';
-        if (children[pos] == null){
-            children[pos] = new TrieNode();
-        }
-        children[pos].insert(word, value, index + 1);
-    }
-    
-    public TrieNode find (String word, int index){
-        if (index == word.length()){
-            return this;
-        }
-        
-        int pos = word.charAt(index) - 'a';
-        if (children[pos] == null){
-            return null;
-        }
-        return children[pos].find(word, index + 1);
-    }
-    
-    public int getSum (){
-        int sum = 0;
-        if (value > 0){
-            sum += value;
-        }
-        for (int i = 0; i < 26; i++){
-            if (children[i] != null){
-                sum += children[i].getSum();
+        public void insert(String s, int value) {
+            TrieNode cur = this.root;
+            for(int i = 0; i < s.length(); i++) {
+                int sid = s.charAt(i) - 'a';
+                if(cur.son[sid] == null) {
+                    cur.son[sid] = new TrieNode();
+                }
+                cur = cur.son[sid];
+                cur.sum += value;
             }
         }
         
-        return sum;
+        public int find(String s) {
+            TrieNode cur = root;
+            for(int i = 0; i < s.length(); i++) {
+                int sid = s.charAt(i) - 'a';
+                if(cur.son[sid] == null) return 0;
+                cur = cur.son[sid];
+            }
+            return cur.sum;
+        }
+        
     }
-}
-
-class MapSum {
-    private TrieNode root;
-
+    
+    public Trie trie;
+    public HashMap<String, Integer> dict;
+    
     /** Initialize your data structure here. */
     public MapSum() {
-        root = new TrieNode();
+        this.trie = new Trie();
+        dict = new HashMap<>();
     }
     
     public void insert(String key, int val) {
-        root.insert(key, val, 0);
+        int old = dict.get(key) == null ? 0 : dict.get(key);
+        dict.put(key, val);
+        this.trie.insert(key, val - old);
     }
     
     public int sum(String prefix) {
-        TrieNode node = root.find(prefix, 0);
-        if (node != null){
-            return node.getSum();
-        }
-        return 0;
+        return this.trie.find(prefix);
     }
 }
 
