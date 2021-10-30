866578ae9791554b1afdcf6cfd6466979868ebd2
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-10-29 22:58:01 -0700

    Update 137_Clone graph.java

--- BFS/137_Clone graph.java
+++ BFS/137_Clone graph.java
@@ -8,6 +8,7 @@
  */
 
 // 边 bfs 边 clone 是不行的，因为每次都会 new 一个新的 neighbor 出来 (丢失遍历过的 node).
+// 主要的思想是 先把所有 node 找到 (BFS) 并放到一个可用 for 循环遍历的数据结构里, 此题选择 List 来存放
 public class Solution {
     /*
      * @param node: A undirected graph node
