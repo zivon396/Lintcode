4bb5aebfb26eff9ab4a575129e1ac39bec012f8c
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-10-30 17:33:02 -0700

    Update 81_Find median from data stream.java

--- Hash - Heap - Queue - Stack/Heap/81_Find median from data stream.java
+++ Hash - Heap - Queue - Stack/Heap/81_Find median from data stream.java
@@ -1,51 +1,50 @@
 public class Solution {
-    /**
-     * @param nums: A list of integers
-     * @return: the median of numbers
-     */
-    private Queue<Integer> maxHeap, minHeap;
-    private int size = 0;
-    
-    private Comparator<Integer> heapComparator = new Comparator<Integer> (){
-        public int compare (Integer left, Integer right){
-            return right - left;
-        }
-    };
-    
-    public int[] medianII(int[] nums) {
-        // write your code here
-        int cnt = nums.length;
-        maxHeap = new PriorityQueue<Integer>(cnt, heapComparator);
-        minHeap = new PriorityQueue<Integer>(cnt);
-        
-        int[] ans = new int[cnt];
-        for (int i = 0; i < cnt; i++){
-            addNumber(nums[i]);
-            ans[i] = getMedian();
-        }
-        
-        return ans;
-    }
-    
-    private void addNumber (int num){
-        maxHeap.add(num);
-        if (size % 2 == 0){
-            if (minHeap.isEmpty()){
-                size++;
-                return;
-            } else if (maxHeap.peek() > minHeap.peek()){
-                Integer maxPeek = maxHeap.poll();
-                Integer minPeek = minHeap.poll();
-                maxHeap.add(minPeek);
-                minHeap.add(maxPeek);
+    PriorityQueue<Integer> minHeap, maxHeap;
+    int median;
+    boolean isFirstAdd;
+    public Solution() {
+        // 小顶堆放大于中位数的数
+        // 大顶堆放小于中位数的数
+        this.minHeap = new PriorityQueue<>();
+        this.maxHeap = 
+            new PriorityQueue<>(new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return b - a;
             }
-        } else {
-            minHeap.add(maxHeap.poll());
-        }
-        size++;
+        });
+        this.isFirstAdd = true;
     }
-    
-    private int getMedian() {
-        return maxHeap.peek();
+
+    public void add(int val) {
+        if (this.isFirstAdd) {
+            // 第一个进入数据流的数字是第一个中位数
+            this.median = val;
+            this.isFirstAdd = false;
+            return ;
+        }
+
+        if (val < this.median) {
+            // 小的数放到大顶堆
+            this.maxHeap.add(val);
+        }
+        else {
+            // 大的数放到小顶堆
+            this.minHeap.add(val);
+        }
+
+        // 比较堆中数字数量，调整堆和中位数
+        if (this.maxHeap.size() > this.minHeap.size()) {
+                this.minHeap.add(this.median);
+                this.median = this.maxHeap.poll();
+        }
+        if (this.maxHeap.size() < this.minHeap.size() - 1) {
+            this.maxHeap.add(this.median);
+            this.median = this.minHeap.poll();
+        }
+    }
+
+    public int getMedian() {
+        // 返回目前维护的中位数
+        return this.median;
     }
 }
