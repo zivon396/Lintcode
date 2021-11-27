201ff2e087fe9aa379938d16d90805120ce376d2
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-11-27 02:54:04 -0800

    Update 1201_Next greater element II.java

--- Hash - Heap - Queue - Stack/Stack/1201_Next greater element II.java
+++ Hash - Heap - Queue - Stack/Stack/1201_Next greater element II.java
@@ -1,16 +1,30 @@
 // 因为是循环数组, double 之后再遍历
 // 也可用 1206 的无 while 版本
-class Solution {
+public class Solution {
+    /**
+     * @param nums: an array
+     * @return: the Next Greater Number for every element
+     */
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length, result[] = new int[n];
-        Arrays.fill(result, -1);
-        Stack<Integer> stack = new Stack<>(); // index stack
-        for (int i = 0; i < n * 2; i++) {
-            int num = nums[i % n]; 
-            while (!stack.isEmpty() && nums[stack.peek()] < num)
-                result[stack.pop()] = num;
+        // Write your code here
+        if (nums == null || nums.length == 0){
+            return new int[] {};
+        }
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        
+        Stack<Integer> stack = new Stack<>(); // Index stack
+        
+        for (int i = 0; i < n * 2; i++){
+            int num = nums[i % n];
+            while (!stack.isEmpty() && num > nums[stack.peek()]){
+                res[stack.pop()] = num;
+            }
+            
             if (i < n) stack.push(i);
-        }   
-        return result;
+        }
+        
+        return res;
     }
 }
