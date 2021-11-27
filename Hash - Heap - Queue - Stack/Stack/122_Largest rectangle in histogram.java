faab8d346aa4422e9ef4506a5b524fc26eefe23d
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-11-27 02:38:52 -0800

    Update 122_Largest rectangle in histogram.java

--- Hash - Heap - Queue - Stack/Stack/122_Largest rectangle in histogram.java
+++ Hash - Heap - Queue - Stack/Stack/122_Largest rectangle in histogram.java
@@ -1,3 +1,6 @@
+// 不严格单调递增 stack
+// 每当遇到下降的柱子, 说明以在它左边比它高的柱子为左边界最多能到这里 => 计算并记录一下矩形面积
+// 注意 edge 处理
 public class Solution {
     /**
      * @param height: A list of integer
@@ -13,9 +16,9 @@ public class Solution {
         int max = 0;
         for (int i = 0; i <= height.length; i++) {
             int curt = (i == height.length) ? -1 : height[i];
-            while (!stack.isEmpty() && curt <= height[stack.peek()]) {
+            while (!stack.isEmpty() && curt < height[stack.peek()]) { // 这里也可是 curt <= height[stack.peek()]
                 int h = height[stack.pop()];
-                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1; // i - (-1) - 1 就是 i
                 max = Math.max(max, h * w);
             }
             stack.push(i);
