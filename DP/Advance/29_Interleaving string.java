aec513bca8b571c1c0d7d3c322eeab0999a14c66
zivon396 <42732539+zivon396@users.noreply.github.com>
2021-11-28 03:43:07 -0800

    Create 29_Interleaving string.java

---
+++ DP/Advance/29_Interleaving string.java
@@ -0,0 +1,37 @@
+// dp[i] 代表由 s1 的前 i 个字母和 s2 的前 j 个字母是否能构成当前 i+j 个字母
+public class Solution {
+    /**
+     * @param s1: A string
+     * @param s2: A string
+     * @param s3: A string
+     * @return: Determine whether s3 is formed by interleaving of s1 and s2
+     */
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        
+        boolean [][] interleaved = new boolean[s1.length() + 1][s2.length() + 1];
+        interleaved[0][0] = true;
+        
+        for (int i = 1; i <= s1.length(); i++) {
+            if(s3.charAt(i - 1) == s1.charAt(i - 1) && interleaved[i - 1][0])
+                interleaved[i][0] = true;
+        }
+        
+        for (int j = 1; j <= s2.length(); j++) {
+            if(s3.charAt(j - 1) == s2.charAt(j - 1) && interleaved[0][j - 1])
+                interleaved[0][j] = true;
+        }
+        
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if(((s3.charAt(i + j - 1) == s1.charAt(i - 1) && interleaved[i - 1][j]))
+                    || ((s3.charAt(i + j - 1)) == s2.charAt(j - 1) && interleaved[i][j - 1]))
+                interleaved[i][j] = true;
+            }
+        }
+        
+        return interleaved[s1.length()][s2.length()];
+    }
+}
