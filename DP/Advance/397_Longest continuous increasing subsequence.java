5d5d25d3afa297fabb556755476039d32ba5c125
zivon396 <42732539+zivon396@users.noreply.github.com>
2022-01-04 00:54:23 -0800

    Create 397_Longest continuous increasing subsequence.java

---
+++ DP/Advance/397_Longest continuous increasing subsequence.java
@@ -0,0 +1,38 @@
+public class Solution {
+    /**
+     * @param A: An array of Integer
+     * @return: an integer
+     */
+    public int longestIncreasingContinuousSubsequence(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        
+        int n = A.length;
+        int answer = 1;
+        
+        // from left to right
+        int length = 1; // just A[0] itself
+        for (int i = 1; i < n; i++) {
+            if (A[i] > A[i - 1]) {
+                length++;
+            } else {
+                length = 1;
+            }
+            answer = Math.max(answer, length);
+        }
+        
+        // from right to left
+        length = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (A[i] > A[i + 1]) {
+                length++;
+            } else {
+                length = 1;
+            }
+            answer = Math.max(answer, length);
+        }
+        
+        return answer;
+    }
+}
