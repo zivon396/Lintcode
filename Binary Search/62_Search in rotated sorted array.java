68ef967f9fb1d551babfcea2a0e31127f85db901
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-06-19 01:10:21 -0700

    Update 62_Search in rotated sorted array.java

--- Binary Search/62_Search in rotated sorted array.java
+++ Binary Search/62_Search in rotated sorted array.java
@@ -14,14 +14,14 @@ public class Solution {
         int start = 0, end = nums.length - 1;
         while (start + 1 < end){
             int mid = start + (end - start) / 2;
-            if (nums[mid] < nums[nums.length - 1]){
-                if (target >= nums[mid] && target <= nums[nums.length - 1]){
+            if (nums[mid] <= nums[nums.length - 1]){
+                if (target <= nums[nums.length - 1] && target >= nums[mid]){
                     start = mid;
                 } else {
                     end = mid;
                 }
             } else {
-                if (target >= nums[0] && target <= nums[mid]){
+                if (target > nums[nums.length - 1] && target <= nums[mid]){
                     end = mid;
                 } else {
                     start = mid;
