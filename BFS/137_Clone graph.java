00b943548e1a1cfff21ae3ad63216e8e620daf93
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-06-20 21:59:13 -0700

    Update 137_Clone graph.java

--- BFS/137_Clone graph.java
+++ BFS/137_Clone graph.java
@@ -7,7 +7,7 @@
  * };
  */
 
-// 边 bfs 边 clone 是不行的，因为每次都会 new 一个新的 neighbor 出来 (丢失遍历过的 node).
+// 边 bfs 边 clone 是不行的，因为每次都会 new 一个新的 neighbor 出来 (丢失遍历过的 node) -> 其实可以的!!! 见方法二
 // 主要的思想是 先把所有 node 找到 (BFS) 并放到一个可用 for 循环遍历的数据结构里, 此题选择 List 来存放
 public class Solution {
     /*
@@ -54,6 +54,50 @@ public class Solution {
             }
         }
         
+        return res;
+    }
+}
+
+// 边 BFS 边 clone (独创):
+public class Solution {
+    /*
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        // write your code here
+        if (node == null){
+            return node;
+        }
+
+        return bfs(node);
+    }
+
+    private UndirectedGraphNode bfs (UndirectedGraphNode node){
+        Queue<UndirectedGraphNode> queue = new LinkedList<>();
+        Set<UndirectedGraphNode> visited = new HashSet<>();
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        UndirectedGraphNode res = new UndirectedGraphNode(node.label);
+        queue.offer(node);
+        map.put(node, res);
+        visited.add(node);
+
+
+        while (!queue.isEmpty()){
+            UndirectedGraphNode curt = queue.poll();
+            UndirectedGraphNode copy = map.get(curt);
+            for (UndirectedGraphNode neighbor: curt.neighbors){
+                UndirectedGraphNode neighborCopy = map.getOrDefault(neighbor, new UndirectedGraphNode(neighbor.label));
+                copy.neighbors.add(neighborCopy);
+
+                if (!visited.contains(neighbor)){
+                    queue.offer(neighbor);
+                    map.put(neighbor, neighborCopy);
+                    visited.add(neighbor);
+                }
+            }
+        }
+
         return res;
     }
 }
