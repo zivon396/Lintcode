2387a1312af92bd101def95e7359bd27d6490bff
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-06-20 22:17:32 -0700

    Update 178_Graph valid tree.java

--- BFS/178_Graph valid tree.java
+++ BFS/178_Graph valid tree.java
@@ -48,3 +48,62 @@ public class Solution {
         return count == n; // 这里也可以直接返回 set.size() == n
     }
 }
+
+// UnionFind
+public class Solution {
+    /**
+     * @param n: An integer
+     * @param edges: a list of undirected edges
+     * @return: true if it's a valid tree, or false
+     */
+    class UnionFind {
+        HashMap<Integer, Integer> father = new HashMap<>();
+        UnionFind (int n){
+            for (int i = 0; i < n; i++){
+                father.put(i, i);
+            }
+        }
+        
+        int compressed_find (int x){
+            int parent = father.get(x);
+            while (parent != father.get(parent)){
+                parent = father.get(parent);
+            }
+            
+            int fa = x;
+            while (fa != father.get(fa)){
+                int tmp = father.get(fa);
+                father.put(fa, parent);
+                fa = tmp;
+            }
+            
+            return parent;
+        }
+        
+        void union (int x, int y){
+            int fa_x = father.get(x);
+            int fa_y = father.get(y);
+            if (fa_x != fa_y){
+                father.put(fa_x, fa_y);
+            }
+        }
+    }
+    
+    public boolean validTree(int n, int[][] edges) {
+        // write your code here
+        if (edges.length != n - 1){
+            return false;
+        }
+        
+        UnionFind uf = new UnionFind(n);
+        
+        for (int i = 0; i < edges.length; i++){
+            if (uf.compressed_find(edges[i][0]) == uf.compressed_find(edges[i][1])) {
+                return false;
+            }
+            uf.union(edges[i][0], edges[i][1]);
+        }
+        
+        return true;
+    }
+}
