ba1389a76e6ec3e2388b7ed080384918d6c35f13
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-06-22 23:47:10 -0700

    Update 93_Balanced binary tree.java

--- Binary tree/93_Balanced binary tree.java
+++ Binary tree/93_Balanced binary tree.java
@@ -49,3 +49,46 @@ public class Solution {
         return new ResultType(true, Math.max(left.maxHeight, right.maxHeight) + 1);
     }
 }
+
+// 原创
+class ResultType {
+    public int depth;
+    public boolean isBalanced;
+    public ResultType(int depth, boolean isBalanced) {
+        this.depth = depth;
+        this.isBalanced = isBalanced;
+    }
+}
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        // write your code here
+        if (root == null){
+            return true;
+        }
+
+        ResultType res = helper(root);
+
+        return res.isBalanced;
+    }
+
+    private ResultType helper (TreeNode root){
+        if (root == null){
+            return new ResultType(0, true);
+        }
+
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+
+        ResultType res = new ResultType(
+            Math.max(left.depth, right.depth) + 1,
+            !(!left.isBalanced || !right.isBalanced || Math.abs(left.depth - right.depth) > 1)
+        );
+
+        return res;
+    }
+}
