3fb35af6afb7a4160903a1ef75a868d532e14a11
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-06-26 21:16:24 -0700

    Update 386_Longest substring with at most k distinct characters.java

--- Two pointers/386_Longest substring with at most k distinct characters.java
+++ Two pointers/386_Longest substring with at most k distinct characters.java
@@ -29,3 +29,39 @@ public class Solution {
         return ans;
     }
 }
+
+// 基本一样
+public class Solution {
+    /**
+     * @param s: A string
+     * @param k: An integer
+     * @return: An integer
+     */
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        // write your code here
+        int res = 0;
+        int[] cnt = new int[256];
+        if (s == null || s.length() == 0){
+            return res;
+        }
+
+        int disdinct = 0;
+        for (int l = 0, r = 0; r < s.length(); r++){
+            if (cnt[s.charAt(r)] == 0){
+                disdinct++;
+            }
+            cnt[s.charAt(r)]++;
+
+            while (disdinct > k){
+                cnt[s.charAt(l)]--;
+                if (cnt[s.charAt(l++)] == 0){
+                    disdinct--;
+                }
+            }
+
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
