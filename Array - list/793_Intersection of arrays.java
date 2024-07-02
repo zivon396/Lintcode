faf0ce09dd84540c71442457af65777017416d8e
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-01 18:44:20 -0700

    Update 793_Intersection of arrays.java

--- Array - list/793_Intersection of arrays.java
+++ Array - list/793_Intersection of arrays.java
@@ -65,3 +65,72 @@ public class Solution {
         return sum;
     }
 }
+
+// version 2:
+class Element {
+    public int row, col;
+    public int val;
+    public Element (int row, int col, int val){
+        this.row = row;
+        this.col = col;
+        this.val = val;
+    }
+}
+
+public class Solution {
+    /**
+     * @param arrs: the arrays
+     * @return: the number of the intersection of the arrays
+     */
+    Comparator<Element> comparator = new Comparator<Element>(){
+        public int compare (Element left, Element right){
+            return left.val - right.val;
+        }
+    };
+
+    public int intersectionOfArrays(int[][] arrs) {
+        // write your code here
+        int res = 0;
+        if (arrs == null || arrs.length == 0){
+            return res;
+        }
+        int n = arrs.length;
+        Queue<Element> heap = new PriorityQueue<>(n, comparator);
+
+        for (int i = 0; i < n; i++){
+            if (arrs[i] != null && arrs[i].length > 0){
+                Arrays.sort(arrs[i]);
+                heap.offer(new Element(
+                    i,
+                    0,
+                    arrs[i][0]
+                ));
+            }
+        }
+
+        int last = -1, curt = 0;
+        int count = 1;
+        while (!heap.isEmpty()){
+            Element ele = heap.poll();
+            curt = ele.val;
+            if (curt != last){
+                last = curt;
+                count = 1;
+            }
+            else {
+                if (++count == n){
+                    res++;
+                }
+            }
+            if (ele.col + 1 < arrs[ele.row].length){
+                heap.offer(new Element(
+                    ele.row,
+                    ele.col + 1,
+                    arrs[ele.row][ele.col + 1]
+                ));
+            }
+        }
+
+        return res;
+    }
+}
