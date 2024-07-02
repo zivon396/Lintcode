b1ab9a9da480526cc92d94c0b692ecd5120dd4d0
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-01 18:18:23 -0700

    Update 577_Merge k sorted interval lists.java

--- Array - list/577_Merge k sorted interval lists.java
+++ Array - list/577_Merge k sorted interval lists.java
@@ -77,3 +77,81 @@ public class Solution {
         return last;
     }
 }
+
+// 不用 last
+class Element {
+    public int x, y;
+    public Interval interval;
+    public Element (int x, int y, Interval interval){
+        this.x = x;
+        this.y = y;
+        this.interval = interval;
+    }
+}
+
+public class Solution {
+    /**
+     * @param intervals: the given k sorted interval lists
+     * @return:  the new sorted interval list
+     */
+    Comparator<Element> comparator = new Comparator<Element>(){
+        public int compare (Element left, Element right){
+            return left.interval.start - right.interval.start;
+        }
+    };
+
+    public List<Interval> mergeKSortedIntervalLists(List<List<Interval>> intervals) {
+        // write your code here
+        List<Interval> res = new ArrayList<>();
+        if (intervals == null || intervals.size() == 0){
+            return res;
+        }
+        int n = intervals.size();
+        Queue<Element> heap = new PriorityQueue<>(n, comparator);
+
+        for (int i = 0; i < n; i++){
+            if (intervals.get(i).size() == 0){
+                continue;
+            }
+            heap.offer(new Element(
+                i,
+                0,
+                intervals.get(i).get(0)
+            ));
+        }
+
+        while (!heap.isEmpty()){
+            Element curt = heap.poll();
+            addRes(res, curt.interval);
+            int x = curt.x, y = curt.y;
+            if (y + 1 < intervals.get(x).size()){
+                heap.offer(new Element(
+                    x,
+                    y + 1,
+                    intervals.get(x).get(y + 1)
+                ));
+            }
+        }
+
+        return res;
+    }
+
+    private void addRes (List<Interval> res, Interval interval){
+        if (res.isEmpty()){
+            res.add(interval);
+        }
+
+        int end = interval.end;
+        Interval pre = res.get(res.size() - 1);
+        if (interval.start <= pre.end){
+            end = Math.max(end, pre.end);
+            res.set(res.size() - 1, new Interval(pre.start, end));
+            return;
+        }
+        else {
+            res.add(interval);
+        }
+
+        return;
+    }
+}
