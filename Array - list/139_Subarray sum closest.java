5098f93ec03e9c26be9d0e23c92d787a9c8ca93b
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-01 19:34:24 -0700

    Update 139_Subarray sum closest.java

--- Array - list/139_Subarray sum closest.java
+++ Array - list/139_Subarray sum closest.java
@@ -39,7 +39,6 @@ public class Solution {
         });
         int ans = Integer.MAX_VALUE;
         for (int i = 1; i <= len; i++) {
-            
             if (ans > sums[i].sum - sums[i-1].sum) {
                 ans = sums[i].sum - sums[i-1].sum;
                 int[] temp = new int[]{sums[i].index - 1, sums[i - 1].index - 1};
@@ -49,6 +48,63 @@ public class Solution {
             }
         }
         
+        return res;
+    }
+}
+
+// 上面的更好 (new Pair[len + 1])
+class Pair {
+    public int index;
+    public int sum;
+    public Pair (int index, int sum){
+        this.index = index;
+        this.sum = sum;
+    }
+}
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number and the index of the last number
+     */
+    Comparator<Pair> comparator = new Comparator<Pair>(){
+        public int compare (Pair left, Pair right){
+            return left.sum - right.sum;
+        }
+    };
+
+    public int[] subarraySumClosest(int[] nums) {
+        // write your code here
+        int[] res = new int[2];
+        int closest = Integer.MAX_VALUE;
+        if (nums == null || nums.length == 0){
+            return res;
+        }
+        int n = nums.length;
+        Pair[] presums = new Pair[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += nums[i];
+            presums[i] = new Pair(i, sum);
+            if (Math.abs(sum) < closest){
+                res[0] = 0;
+                res[1] = i;
+                closest = Math.abs(sum);
+            }
+        }
+
+        Arrays.sort(presums, comparator);
+        for (int i = 1; i < n; i++){
+            int diff = presums[i].sum - presums[i - 1].sum;
+            if (diff < closest){
+                res[0] = presums[i].index;
+                res[1] = presums[i - 1].index;
+                Arrays.sort(res);
+                res[0]++;
+                closest = diff;
+            }
+        }
+
         return res;
     }
 }
