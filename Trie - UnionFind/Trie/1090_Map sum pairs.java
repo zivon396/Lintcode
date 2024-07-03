c7681be640edcf9381916ac70bb671ed24c8bbef
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-02 22:05:30 -0700

    Update 1090_Map sum pairs.java

--- Trie - UnionFind/Trie/1090_Map sum pairs.java
+++ Trie - UnionFind/Trie/1090_Map sum pairs.java
@@ -60,3 +60,75 @@ class MapSum {
  * obj.insert(key,val);
  * int param_2 = obj.sum(prefix);
  */
+
+
+// version 2
+class TrieNode {
+    private TrieNode[] children;
+    public int value;
+    
+    public TrieNode (){
+        children = new TrieNode[26];
+        value = 0;
+    }
+    
+    public void insert (String word, int value, int index){
+        if (index == word.length()){
+            this.value = value;
+            return;
+        }
+        
+        int pos = word.charAt(index) - 'a';
+        if (children[pos] == null){
+            children[pos] = new TrieNode();
+        }
+        children[pos].insert(word, value, index + 1);
+    }
+    
+    public TrieNode find (String word, int index){
+        if (index == word.length()){
+            return this;
+        }
+        
+        int pos = word.charAt(index) - 'a';
+        if (children[pos] == null){
+            return null;
+        }
+        return children[pos].find(word, index + 1);
+    }
+    
+    public int getSum (){
+        int sum = 0;
+        if (value > 0){
+            sum += value;
+        }
+        for (int i = 0; i < 26; i++){
+            if (children[i] != null){
+                sum += children[i].getSum();
+            }
+        }
+        
+        return sum;
+    }
+}
+
+class MapSum {
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public MapSum() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String key, int val) {
+        root.insert(key, val, 0);
+    }
+    
+    public int sum(String prefix) {
+        TrieNode node = root.find(prefix, 0);
+        if (node != null){
+            return node.getSum();
+        }
+        return 0;
+    }
+}
