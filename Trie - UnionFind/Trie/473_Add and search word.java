f558e88e0d2bb62728632cf7ecce6c0429a0e258
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-02 23:35:02 -0700

    Update 473_Add and search word.java

--- Trie - UnionFind/Trie/473_Add and search word.java
+++ Trie - UnionFind/Trie/473_Add and search word.java
@@ -139,3 +139,85 @@ public class WordDictionary {
         return false;
     }
 }
+
+
+// version 3: find 写在 Node 里, add 写在 Trie 里
+class Node {
+    public boolean isWord;
+    public Node[] children;
+    public Node (){
+        this.isWord = false;
+        this.children = new Node[26];
+    }
+
+    public boolean find (String word, int index){
+        if (index == word.length()){
+            return this.isWord;
+        }
+
+        if (word.charAt(index) == '.'){
+            for (Node node: this.children){
+                if (node != null && node.find(word, index + 1)){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        else {
+            int pos = word.charAt(index) - 'a';
+            if (this.children[pos] == null){
+                return false;
+            }
+
+            return this.children[pos].find(word, index + 1);
+        }
+    }
+}
+
+class Trie {
+    private Node root;
+    public Trie (){
+        this.root = new Node();
+    }
+
+    public void add (String word){
+        Node now = root;
+        int n = word.length();
+        for (int i = 0; i < n; i++){
+            int pos = word.charAt(i) - 'a';
+            if (now.children[pos] == null){
+                now.children[pos] = new Node();
+            }
+            now = now.children[pos];
+        }
+
+        now.isWord = true;
+    }
+
+    public boolean find (String word){
+        return this.root.find(word, 0);
+    }
+}
+
+public class WordDictionary {
+    /*
+     * @param word: Adds a word into the data structure.
+     * @return: nothing
+     */
+    public Trie trie = new Trie();
+
+    public void addWord(String word) {
+        // write your code here
+        trie.add(word);
+    }
+
+    /*
+     * @param word: A word could contain the dot character '.' to represent any one letter.
+     * @return: if the word is in the data structure.
+     */
+    public boolean search(String word) {
+        // write your code here
+        return trie.find(word);
+    }
+}
