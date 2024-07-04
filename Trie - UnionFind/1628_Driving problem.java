2c22aef423fff471a26c1b2b8fd534704fe91489
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-03 22:11:24 -0700

    Update 1628_Driving problem.java

--- Trie - UnionFind/1628_Driving problem.java
+++ Trie - UnionFind/1628_Driving problem.java
@@ -1,57 +1,74 @@
 // 若上下两堵墙可以被连通, 则不可通过; 反之则可以
 // 上下两堵墙分别连结到 size + 1 和 size + 2
+class UnionFind {
+    int[] father;
+  
+    public UnionFind(int n) {
+        father = new int[n];
+        for (int i = 0; i < n; i++){
+            father[i] = i;
+        }
+    }
+
+    public int find (int x){
+        if (father[x] == x){
+            return x;
+        }
+        return father[x] = find(father[x]);
+    }
+
+    public void union(int a, int b) {
+        // write your code here
+        int root_a = find(a);
+        int root_b = find(b);
+        if (root_a != root_b){
+            father[root_a] = root_b;
+        }
+    }
+
+    public boolean isConnected(int a, int b) {
+        return find(a) == find(b);
+    }
+}
+
 public class Solution {
-    public String drivingProblem(int L, int W, double[][] p) {
-        if (L <= 4 || W <= 4) {
+    /**
+     * @param l: the length
+     * @param w: the width
+     * @param p:  the obstacle coordinates
+     * @return: yes or no
+     */
+    public String drivingProblem(int l, int w, double[][] p) {
+        // Write your code here.
+        if (l <= 4 || w <= 4){
             return "no";
         }
-        if (p == null || p.length == 0) {
+        if (p == null || p.length == 0){
             return "yes";
         }
         int size = p.length;
-        Unionfind uf = new Unionfind(size + 2);
-        for (int i = 0; i < size; i++) {
+        UnionFind uf = new UnionFind(size + 2);
+        for (int i = 0; i < size; i++){
             if (p[i][1] <= 5) {
-                uf.connect(i, size);
+                uf.union(i, size);
             }
-            if (W - p[i][1] <= 5) {
-                uf.connect(i, size + 1);
+            if (w - p[i][1] <= 5) {
+                uf.union(i, size + 1);
             }
         }
-        
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                if ((p[i][0] - p[j][0]) * (p[i][0] - p[j][0]) + (p[i][1] - p[j][1]) * (p[i][1] - p[j][1]) <= 36.0) {
-                    uf.connect(i, j);
+
+        for (int i = 0; i < size; i++){
+            for (int j = i + 1; j < size; j++){
+                if (getDistance(p[i][0], p[i][1], p[j][0], p[j][1]) <= 6 * 6){
+                    uf.union(i, j);
                 }
             }
         }
-        return (uf.find(size) == uf.find(size + 1)) ? "no" : "yes";
+
+        return uf.isConnected(size, size + 1) ? "no" : "yes";
     }
-    
-    class Unionfind {
-        int[] father;
-        
-        Unionfind(int size) {
-            father = new int[size];
-            for (int i = 0; i < size; i++) {
-                father[i] = i;
-            }
-        }
-        
-        int find(int x) {
-            if (father[x] == x) {
-                return x;
-            }
-            return father[x] = find(father[x]);
-        }
-        
-        void connect(int x, int y) {
-            int fx = find(x);
-            int fy = find(y);
-            if (fx != fy) {
-                father[fy] = fx;
-            }
-        }
+
+    private double getDistance (double x1, double y1, double x2, double y2){
+        return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
     }
 }
