14fbc6fad356cfabfa1af23732e8cc1d810cc7b9
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-05 21:36:39 -0700

    Update 122_Largest rectangle in histogram.java

--- Hash - Heap - Queue - Stack/Stack/122_Largest rectangle in histogram.java
+++ Hash - Heap - Queue - Stack/Stack/122_Largest rectangle in histogram.java
@@ -1,7 +1,7 @@
 // 不严格单调递增 stack
 // 每当遇到下降的柱子, 说明以在它左边比它高的柱子为左边界最多能到这里 => 计算并记录一下矩形面积
 // 每 pop 出一个, 就把 pop 出的那个柱子削减成和 curt 一样高
-// 模板背过: while 的判断里面是 peek; while 里先 pop, 再用 peek 计算 w (i - peek - 1) => 363 也一样
+// 模板背过 (363 同): while 的判断里面是 peek; while 里先 pop, 再用 peek 计算 w (i - peek - 1) => 从 peek 的下一个开始算
 // 注意 edge 处理
 public class Solution {
     /**
