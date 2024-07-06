aa4131dd9dd02ba15bb7a73d3c636e3f1ac6c6d4
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-05 20:34:24 -0700

    Update 575_Decode string.java

--- Hash - Heap - Queue - Stack/Stack/575_Decode string.java
+++ Hash - Heap - Queue - Stack/Stack/575_Decode string.java
@@ -1,3 +1,5 @@
+// 一个 stack 就可以
+// push String 就行, 不需要 char
 public class Solution {
     /**
      * @param s: an expression includes numbers, letters and brackets
@@ -49,5 +51,58 @@ public class Solution {
             return true;
         }
         return false;
+    }
+}
+
+// 原创
+public class Solution {
+    /**
+     * @param s: an expression includes numbers, letters and brackets
+     * @return: a string
+     */
+    public String expressionExpand(String s) {
+        // write your code here
+        if (s == null || s.length() == 0){
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++){
+            if (chars[i] != ']'){
+                stack.push(chars[i]);
+            }
+            else {
+                String now = "";
+                while (!stack.isEmpty() && Character.isLetter(stack.peek())){
+                    now = Character.toString(stack.pop()) + now;
+                }
+                stack.pop();
+
+                String multi = "";
+                while (!stack.isEmpty() && Character.isDigit(stack.peek())){
+                    multi = Character.toString(stack.pop()) + multi;
+                }
+                int time = Integer.parseInt(multi);
+                String tmp = now;
+                for (int j = 0; j < time - 1; j++){
+                    now += tmp;
+                }
+                if (time == 0){
+                    now = "";
+                }
+
+                for (int j = 0; j < now.length(); j++){
+                    stack.push(now.charAt(j));
+                }
+            }
+        }
+
+        String res = "";
+        while (!stack.isEmpty()){
+            res = Character.toString(stack.pop()) + res;
+        }
+
+        return res;
     }
 }
