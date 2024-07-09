06f7ad5d37f3eebe8586c950f4528dacf7600b70
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-09 01:22:04 -0700

    Update 110_Minimum Path Sum.java

--- DP/110_Minimum Path Sum.java
+++ DP/110_Minimum Path Sum.java
@@ -34,3 +34,38 @@ public class Solution {
         return f[m - 1][n - 1];
     }
 }
+
+// version 2
+public class Solution {
+    /**
+     * @param grid: a list of lists of integers
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    public int minPathSum(int[][] grid) {
+        // write your code here
+        if (grid == null || grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        int n = grid.length, m = grid[0].length;
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }
+                else if (i == 0){
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                }
+                else if (j == 0){
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+
+        return dp[n - 1][m - 1];
+    }
+}
