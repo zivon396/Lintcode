ac61ecfa4364e8876e3e4025d41a4e60dce02152
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-10 03:06:02 -0700

    Update 513_Perfect squares.java

--- DP/513_Perfect squares.java
+++ DP/513_Perfect squares.java
@@ -23,3 +23,30 @@ public class Solution {
         return dp[n];
     }
 }
+
+
+// 四平方和定理:
+public class Solution {
+    /**
+     * @param n a positive integer
+     * @return an integer
+     */
+    public int numSquares(int n) {
+        while (n % 4 == 0)
+            n /= 4;
+        if (n % 8 == 7)
+            return 4;
+        for (int i = 0; i * i <= n; ++i) {
+            int j = (int)Math.sqrt(n * 1.0 - i * i);
+            if (i * i + j * j == n) {
+                int res = 0;
+                if (i > 0)
+                    res += 1;
+                if (j > 0)
+                    res += 1;
+                return res;
+            }
+        }
+        return 3;
+    }
+}
