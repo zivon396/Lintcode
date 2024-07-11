702e7a5866852412754132b55a837864b7b8a4d2
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-11 01:40:21 -0700

    Update 44_Minimum subarray.java

--- DP/Advance/44_Minimum subarray.java
+++ DP/Advance/44_Minimum subarray.java
@@ -20,3 +20,27 @@ public class Solution {
         return ans;
     }
 }
+
+// globalMin + localMin:
+public class Solution {
+    /*
+     * @param nums: a list of integers
+     * @return: A integer indicate the sum of minimum subarray
+     */
+    public int minSubArray(List<Integer> nums) {
+        // write your code here
+        if (nums == null || nums.size() == 0) {
+            return 0;
+        }
+        int n = nums.size();
+        int []localMin = new int[n]; 
+        int []golbalMin = new int[n];
+        localMin[0] = golbalMin[0] = nums.get(0);
+        for(int i = 1; i < n; i++){
+            localMin[i] = Math.min(localMin[i - 1] + nums.get(i), nums.get(i)); //比较判断是否从一个新的元素开始找SubArray
+            golbalMin[i] = Math.min(golbalMin[i - 1], localMin[i]);// 前 i 位元素可以构成的最小subArray之和
+        }
+        
+        return golbalMin[n - 1];
+    }
+}
