8df941ef099b43af81f1885ba320c382021d9eb7
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-10 21:10:10 -0700

    Update 630_Knight shortest Path II.java

--- DP/630_Knight shortest Path II.java
+++ DP/630_Knight shortest Path II.java
@@ -39,3 +39,52 @@ public class Solution {
         return f[n - 1][m - 1];
     }
 }
+
+// 自创
+public class Solution {
+    /**
+     * @param grid: a chessboard included 0 and 1
+     * @return: the shortest path
+     */
+    private int[] dirX = new int[]{1, -1, 2, -2};
+    private int[] dirY = new int[]{2, 2, 1, 1};
+    public int shortestPath2(boolean[][] grid) {
+        // write your code here
+        if (grid == null || grid.length == 0 ||grid[0].length == 0){
+            return 0;
+        }
+        int n = grid.length, m = grid[0].length;
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++){
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
+        for (int j = 0; j < m; j++){
+            for (int i = 0; i < n; i++){
+                if (i == 0 && j == 0){
+                    dp[i][j] = !grid[i][j] ? 0 : Integer.MAX_VALUE;
+                    continue;
+                }
+
+                if (grid[i][j]){
+                    continue;
+                }
+
+                for (int k = 0; k < 4; k++){
+                    int newX = i - dirX[k];
+                    int newY = j - dirY[k];
+                    if (isValid(grid, n, m, newX, newY) && dp[newX][newY] != Integer.MAX_VALUE){
+                        dp[i][j] = Math.min(dp[newX][newY] + 1, dp[i][j]);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1][m - 1] == Integer.MAX_VALUE ? -1 : dp[n - 1][m - 1];
+    }
+
+    private boolean isValid (boolean[][] grid, int n, int m, int x, int y){
+        return x >= 0 && x < n && y >= 0 && y < m && !grid[x][y];
+    }
+}
