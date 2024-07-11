fa3a8a022b551bfa7c4b50c016df16ab52ea32af
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-11 01:15:29 -0700

    Update 392_House robber.java

--- DP/Advance/392_House robber.java
+++ DP/Advance/392_House robber.java
@@ -24,3 +24,30 @@ public class Solution {
         return res[n % 2];
     }
 }
+
+// version 2 (原创)
+public class Solution {
+    /**
+     * @param a: An array of non-negative integers
+     * @return: The maximum amount of money you can rob tonight
+     */
+    public long houseRobber(int[] a) {
+        // write your code here
+        if (a == null || a.length == 0){
+            return 0;
+        }
+        if (a.length < 2){
+            return a[0];
+        }
+        int n = a.length;
+        long[] dp = new long[2];
+        dp[0] = a[0];
+        dp[1] = Math.max(a[0], a[1]);
+
+        for (int i = 2; i < n; i++){
+            dp[i % 2] = Math.max(dp[(i - 1) % 2], dp[(i - 2) % 2] + a[i]);
+        }
+
+        return Math.max(dp[0], dp[1]);
+    }
+}
