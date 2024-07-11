88aacde585ba75e6a624dd36657e5aed75c25e6a
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-10 23:44:30 -0700

    Update 602_Russian Doll Envelopes.java

--- DP/602_Russian Doll Envelopes.java
+++ DP/602_Russian Doll Envelopes.java
@@ -44,3 +44,96 @@ public class Solution {
 // DP + Binary Search
 // 先将信封在第一个维度上排序, 然后在第二个维度上做 Longest increasing subsequence
 // 注意: comparator 的第二个维度必须反着排, 因为第一个维度相同的话是不能嵌套的
+public class Solution {
+    /**
+     * @param envelopes: a number of envelopes with widths and heights
+     * @return: the maximum number of envelopes
+     */
+    private Comparator<int[]> arrayCom = new Comparator<int[]>(){
+        public int compare (int[] left, int[] right){
+            if (left[0] == right[0]){
+                return right[1] - left[1];
+            }
+
+            return left[0] - right[0];
+        }
+    };
+
+    public int maxEnvelopes(int[][] envelopes) {
+        // write your code here
+        if (envelopes == null || envelopes.length == 0 || envelopes[0].length == 0){
+            return 0;
+        }
+        int n = envelopes.length;
+        Arrays.sort(envelopes, arrayCom);
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        for (int i = 0; i < n; i++){
+            int[] envelope = envelopes[i];
+            int index = binarySearch(dp, envelope[1]);
+            dp[index] = envelope[1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (dp[i] != Integer.MAX_VALUE) {
+                return i + 1;
+            }
+        }
+
+        return 0;
+    }
+
+    private int binarySearch (int[] dp, int target){
+        int start = 0, end = dp.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (dp[mid] >= target){
+                end = mid;
+            }
+            else {
+                start = mid;
+            }
+        }
+
+        if (dp[start] >= target){
+            return start;
+        }
+
+        return end;
+    }
+}
+
+// DP + Binary Search version 2:
+// 直接利用 Arrays.binarySearch()
+public class Solution {
+    /**
+     * @param envelopes a number of envelopes with widths and heights
+     * @return the maximum number of envelopes
+     */
+    public int maxEnvelopes(int[][] envelopes) {
+        // Write your code here
+        if(envelopes == null || envelopes.length == 0 
+            || envelopes[0] == null || envelopes[0].length != 2)
+            return 0;
+        Arrays.sort(envelopes, new Comparator<int[]>(){
+            public int compare(int[] arr1, int[] arr2){
+                if(arr1[0] == arr2[0])
+                    return arr2[1] - arr1[1];
+                else
+                    return arr1[0] - arr2[0];
+            } 
+        });
+        int dp[] = new int[envelopes.length];
+        int len = 0;
+        for(int[] envelope : envelopes){
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+                if(index < 0)
+                    index = -index - 1;
+            dp[index] = envelope[1];
+            if (index == len)
+                len++;
+        }
+        return len;
+    }
+}
