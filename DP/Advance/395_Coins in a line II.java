94f77b6db113945e170b70ba13c778d748856cfa
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-12 10:31:13 -0700

    Update 395_Coins in a line II.java

--- DP/Advance/395_Coins in a line II.java
+++ DP/Advance/395_Coins in a line II.java
@@ -34,6 +34,35 @@ public class Solution {
     }
 }
 
+// dp[i] => 现在还剩 i 个硬币, 当前取硬币的人可以赢得的最大分差
+public class Solution {
+    /**
+     * @param values: a vector of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int[] values) {
+        // write your code here
+        if (values == null || values.length == 0){
+            return false;
+        }
+        if (values.length < 3){
+            return true;
+        }
+
+        int n = values.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = values[n - 1];
+        dp[2] = values[n - 1] + values[n - 2];
+
+        for (int i = 3; i <= n; i++){
+            dp[i] = Math.max(values[n - i] - dp[i - 1], values[n - i] + values[n - i + 1] - dp[i - 2]);
+        }
+
+        return dp[n] >= 0;
+    }
+}
+
 // dp[i] => 现在还剩 i 个硬币, 当前先手取硬币的人最后最多取到的价值
 // dp[i] = max(min(dp[i-2], dp[i-3]) + coin[n-i], min(dp[i-3], dp[i-4]) + coin[n-i] + coin[n-i+1])
 public class Solution {
