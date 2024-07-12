5c7b6dbcc205d1c71d29c99bbfb2cb5b09740048
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-12 03:09:02 -0700

    Update 436_Maximal square.java

--- DP/Advance/436_Maximal square.java
+++ DP/Advance/436_Maximal square.java
@@ -35,3 +35,44 @@ public class Solution {
         return ans * ans;
     }
 }
+
+// version 2 (更好):
+public class Solution {
+    /**
+     * @param matrix: a matrix of 0 and 1
+     * @return: an integer
+     */
+    public int maxSquare(int[][] matrix) {
+        // write your code here
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return 0;
+        }
+        int n = matrix.length, m = matrix[0].length;
+        int[][] dp = new int[2][m];
+        int max = 0;
+        for (int i = 0; i < m; i++){
+            dp[0][i] = matrix[0][i];
+            max = Math.max(max, dp[0][i]);
+        }
+
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (matrix[i][j] > 0){
+                    if (j == 0){
+                        dp[i % 2][j] = 1;
+                    }
+                    else {
+                        dp[i % 2][j] = Math.min(Math.min(dp[(i - 1) % 2][j], dp[i % 2][j - 1]), dp[(i - 1) % 2][j - 1]) + 1;
+                    }
+                }
+                else {
+                    dp[i % 2][j] = 0;
+                }
+
+                max = Math.max(max, dp[i % 2][j]);
+            }
+        }
+
+        return max * max;
+    }
+}
