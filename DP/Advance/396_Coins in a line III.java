a972338ff35065b0b3b0da0c879532572ad8c717
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-12 10:06:26 -0700

    Update 396_Coins in a line III.java

--- DP/Advance/396_Coins in a line III.java
+++ DP/Advance/396_Coins in a line III.java
@@ -29,3 +29,37 @@ public class Solution {
         return dp[0][n - 1] >= 0;
     }
 }
+
+// dp[i][j] 现在还剩从第 i 到第 j 个硬币, 当前选取的玩家能最后取得的最大价值
+public class Solution {
+    /**
+     * @param values: an array of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int[] values) {
+        // write your code here
+
+        int n = values.length;
+        int[] sum = new int[n + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= n; ++i)
+            sum[i] = sum[i - 1] + values[i - 1];
+
+        // s[i][j] = sum[j + 1] - sum[i];
+
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; ++i)
+            dp[i][i] = values[i];
+
+        for (int len = 2; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                int j = i + len - 1;
+                int s = sum[j + 1] - sum[i];
+                
+                dp[i][j] = Math.max(s - dp[i + 1][j], s - dp[i][j - 1]);
+            }
+        }
+
+        return dp[0][n - 1] > sum[n] / 2;
+    }
+}
