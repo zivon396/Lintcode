bb9ea26e4ee806d4084c73e05996a72c6531913e
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-12 02:26:04 -0700

    Update 191_Maximum product subarray.java

--- DP/Advance/191_Maximum product subarray.java
+++ DP/Advance/191_Maximum product subarray.java
@@ -27,3 +27,43 @@ public class Solution {
         return result;
     }
 }
+
+// 原创版本 (更顺眼)
+public class Solution {
+    /**
+     * @param nums: An array of integers
+     * @return: An integer
+     */
+    public int maxProduct(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        if (nums.length == 1){
+            return nums[0];
+        }
+        int n = nums.length;
+        int[] min = new int[2];
+        int[] max = new int[2];
+        min[0] = nums[0];
+        max[0] = nums[0];
+        min[1] = Math.min(nums[1], nums[0] * nums[1]);
+        max[1] = Math.max(nums[1], nums[0] * nums[1]);
+        int result = Math.max(max[0], max[1]);
+
+        for (int i = 2; i < n; i++){
+            if (nums[i] > 0){
+                max[i % 2] = Math.max(max[(i - 1) % 2] * nums[i], nums[i]);
+                min[i % 2] = Math.min(min[(i - 1) % 2] * nums[i], nums[i]);
+            }
+            else {
+                max[i % 2] = Math.max(min[(i - 1) % 2] * nums[i], nums[i]);
+                min[i % 2] = Math.min(max[(i - 1) % 2] * nums[i], nums[i]);
+            }
+
+            result = Math.max(result, max[i % 2]);
+        }
+
+        return result;
+    }
+}
