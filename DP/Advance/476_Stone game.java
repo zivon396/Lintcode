72f5dfa72618681e91ccdc20f602e15e1a75c3b6
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-13 02:48:30 -0700

    Update 476_Stone game.java

--- DP/Advance/476_Stone game.java
+++ DP/Advance/476_Stone game.java
@@ -2,53 +2,50 @@
 // dp[i][j] = min(dp[i][k] + dp[k + 1][j] + sum[i][j])
 public class Solution {
     /**
-     * @param A: An integer array
+     * @param a: An integer array
      * @return: An integer
      */
-    int search(int l, int r, int[][] f, int[][] visit, int[][] sum) {
-        
-        if(visit[l][r] == 1)
-            return f[l][r];
-        if(l == r) {
-            visit[l][r] = 1;
-            return f[l][r];
-        }
-        
-        f[l][r] = Integer.MAX_VALUE;
-        for (int k = l; k < r; k++) {
-            f[l][r] = Math.min(f[l][r], search(l, k, f, visit, sum) + search(k + 1, r, f, visit, sum) + sum[l][r]);
-        }
-
-        visit[l][r] = 1;
-        
-        return f[l][r];
-    }
-    
-    public int stoneGame(int[] A) {
-        if (A == null || A.length == 0) {
+    public int stoneGame(int[] a) {
+        // write your code here
+        if (a == null || a.length == 0){
             return 0;
         }
-        
-        int n = A.length;
-        
-        // initialize
-        int[][] f = new int[n][n];
-        int[][] visit = new int[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            f[i][i] = 0;
-        }
-        
-        // preparation
+        int n = a.length;
+        int[][] dp = new int[n][n];
+        int[][] visited = new int[n][n];
         int[][] sum = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            sum[i][i] = A[i];
-            for (int j = i + 1; j < n; j++) {
-                sum[i][j] = sum[i][j - 1] + A[j];
+
+        for (int i = 0; i < n; i++){
+            dp[i][i] = 0;
+        }
+
+        for (int i = 0; i < n; i++){
+            sum[i][i] = a[i];
+            for (int j = i + 1; j < n; j++){
+                sum[i][j] = sum[i][j - 1] + a[j];
             }
         }
-        
-        return search(0, n-1, f, visit, sum);
-        
+
+        int min = memorySearch(dp, visited, sum, 0, n - 1);
+
+        return min;
+    }
+
+    private int memorySearch (int[][] dp, int[][] visited, int[][] sum, int start, int end){
+        if (visited[start][end] == 1){
+            return dp[start][end];
+        }
+        if (start == end){
+            visited[start][end] = 1;
+            return dp[start][end];
+        }
+
+        visited[start][end] = 1;
+        dp[start][end] = Integer.MAX_VALUE;
+        for (int k = start; k < end; k++){
+            dp[start][end] = Math.min(dp[start][end], memorySearch(dp, visited, sum, start, k) + memorySearch(dp, visited, sum, k + 1, end) + sum[start][end]);
+        }
+
+        return dp[start][end];
     }
 }
