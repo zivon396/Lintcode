a00cd5c2b0c2d6ca290c17e663c199b3442be211
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-13 04:44:18 -0700

    Update 168_Burst balloons.java

--- DP/Advance/168_Burst balloons.java
+++ DP/Advance/168_Burst balloons.java
@@ -1,3 +1,5 @@
+// 记忆化搜索
+// 把原数组扩展一下更方便
 public class Solution {
     /**
      * @param nums: A list of integer
@@ -5,30 +7,32 @@ public class Solution {
      */
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int [][]dp = new int [n+2][n+2];
-        int [][]visit = new int[n+2][n+2]; 
-        int [] arr = new int [n+2];
+        int[][] dp = new int[n + 2][n + 2];
+        int[][] visit = new int[n + 2][n + 2]; 
+        int[] arr = new int[n + 2];
         for (int i = 1; i <= n; i++){
-            arr[i] = nums[i-1];
+            arr[i] = nums[i - 1];
         }
         arr[0] = 1;
-        arr[n+1] = 1;
+        arr[n + 1] = 1;
         
         return search(arr, dp, visit, 1 , n);
     }
-    public int search(int []arr, int [][]dp, int [][]visit, int left, int right) {
-        if(visit[left][right] == 1)
+    public int search(int[] arr, int[][] dp, int[][] visit, int left, int right) {
+        if (visit[left][right] == 1)
         	return dp[left][right];
     	
     	int res = 0;
-        for (int k = left; k <= right; ++k) {
-        	int midValue =  arr[left - 1] * arr[k] * arr[right + 1];
+        for (int k = left; k <= right; ++k){
+        	int midValue = arr[left - 1] * arr[k] * arr[right + 1];
         	int leftValue = search(arr, dp, visit, left, k - 1);
         	int rightValue = search(arr, dp, visit, k + 1, right);
             res = Math.max(res, leftValue + midValue + rightValue);
         }
+
         visit[left][right] = 1;
         dp[left][right] = res;
+
         return res;
     }
 }
