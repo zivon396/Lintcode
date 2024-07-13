11d3815819e70444c969aab8c3846fb60eb29a1d
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-13 03:20:08 -0700

    Update 593_Stone game II.java

--- DP/Advance/593_Stone game II.java
+++ DP/Advance/593_Stone game II.java
@@ -41,3 +41,62 @@ public class Solution {
         
     }
 }
+
+// 记忆化搜索版本
+public class Solution {
+    /**
+     * @param a: An integer array
+     * @return: An integer
+     */
+    public int stoneGame2(int[] a) {
+        // write your code here
+        if (a == null || a.length == 0){
+            return 0;
+        }
+        int n = a.length;
+        int[] aa = new int[n * 2];
+        int[][] dp = new int[n * 2][n * 2];
+        int[][] visited = new int[n * 2][n * 2];
+        int[][] sum = new int[n * 2][n * 2];
+
+        for (int i = 0; i < n * 2; i++){
+            aa[i] = a[i % n];
+        }
+
+        for (int i = 0; i < n * 2; i++){
+            dp[i][i] = 0;
+        }
+
+        for (int i = 0; i < n * 2; i++){
+            sum[i][i] = aa[i];
+            for (int j = i + 1; j < n * 2; j++){
+                sum[i][j] = sum[i][j - 1] + a[j % n];
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++){
+            min = Math.min(min, memorySearch(dp, visited, sum, 0 + i, n + i - 1));
+        }
+
+        return min;
+    }
+
+    private int memorySearch (int[][] dp, int[][] visited, int[][] sum, int start, int end){
+        if (visited[start][end] == 1){
+            return dp[start][end];
+        }
+        if (start == end){
+            visited[start][end] = 1;
+            return dp[start][end];
+        }
+
+        visited[start][end] = 1;
+        dp[start][end] = Integer.MAX_VALUE;
+        for (int k = start; k < end; k++){
+            dp[start][end] = Math.min(dp[start][end], memorySearch(dp, visited, sum, start, k) + memorySearch(dp, visited, sum, k + 1, end) + sum[start][end]);
+        }
+
+        return dp[start][end];
+    }
+}
