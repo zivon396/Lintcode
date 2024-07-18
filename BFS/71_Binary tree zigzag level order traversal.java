c09aa7755031304249748b0fd91b9383b695eef1
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-17 17:04:12 -0700

    Update 71_Binary tree zigzag level order traversal.java

--- BFS/71_Binary tree zigzag level order traversal.java
+++ BFS/71_Binary tree zigzag level order traversal.java
@@ -52,6 +52,45 @@ public class Solution {
 }
 
 // Dequeue
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        nodeQueue.offer(root);
+        boolean isOrderLeft = true;
+
+        while (!nodeQueue.isEmpty()) {
+            Deque<Integer> levelList = new LinkedList<Integer>();
+            int size = nodeQueue.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode curNode = nodeQueue.poll();
+                if (isOrderLeft) {
+                    levelList.offerLast(curNode.val); // 放在队列末尾
+                } else {
+                    levelList.offerFirst(curNode.val); // 放在队列前端
+                }
+                
+                if (curNode.left != null) {
+                    nodeQueue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    nodeQueue.offer(curNode.right);
+                }
+            }
+            ans.add(new LinkedList<Integer>(levelList));
+            isOrderLeft = !isOrderLeft;
+        }
+
+        return ans;
+    }
+}
+
+
+// 下面这个不太对, 但还是学习一下 ArrayDeque
 public class Solution {
     /**
      * @param root: A Tree
