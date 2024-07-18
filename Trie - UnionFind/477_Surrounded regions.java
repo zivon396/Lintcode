de08d1f151f9f111d25f67126c931287ddb92e08
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-17 17:22:54 -0700

    Update 477_Surrounded regions.java

--- Trie - UnionFind/477_Surrounded regions.java
+++ Trie - UnionFind/477_Surrounded regions.java
@@ -158,3 +158,62 @@ public class Solution {
         }
     }
 }
+
+
+// DFS 版本
+public class Solution {
+    /**
+     * @param board a 2D board containing 'X' and 'O'
+     * @return void
+     */
+    public void surroundedRegions(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return ;
+        }
+        int n = board.length,m = board[0].length;
+        //四周向中间搜
+        for (int i = 0; i < n;i++) {
+            for (int j = 0;j < m;j++) {
+                if (!is_border(i,j,n,m)) {
+                    continue;
+                }
+                if (board[i][j] == 'O') {
+                    dfs(board , i , j);
+                }
+            }
+        }
+        
+        //遍历图，更新结果
+        for (int i = 0;i < n;i++) {
+            for (int j = 0;j < m;j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                else if (board[i][j] == '*') {
+                    board[i][j] = 'O'; 
+                }
+            }
+        }
+    }
+    
+    public void dfs(char[][] board , int x , int y){
+        //保证当前点在图中
+        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length) {
+            return ;
+        }
+        if (board[x][y] != 'O') {
+            return ;
+        }
+        board[x][y] = '*';
+        dfs(board , x + 1, y);
+        dfs(board , x - 1, y);
+        dfs(board , x , y + 1);
+        dfs(board , x , y - 1);
+    }
+    public boolean is_border(int x,int y,int n,int m) {//判断是否边界
+        if (x == 0 || y == 0 || x == n - 1||y == m - 1) {
+            return true;
+        }
+        return false;
+    }
+}
