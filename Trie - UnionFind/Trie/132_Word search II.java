798ed99decb8ee0f8f9c4b1d37d68e0bc24c890a
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-21 12:40:40 -0700

    Update 132_Word search II.java

--- Trie - UnionFind/Trie/132_Word search II.java
+++ Trie - UnionFind/Trie/132_Word search II.java
@@ -1,4 +1,5 @@
-//version 1:
+// version 1
+// TrieNode 不自调
 class TrieNode {
     String word;
     HashMap<Character, TrieNode> children;
@@ -7,7 +8,6 @@ class TrieNode {
         children = new HashMap<Character, TrieNode>();
     }
 };
-
 
 class TrieTree{
     TrieNode root;
@@ -93,8 +93,8 @@ public class Solution {
     }
 }
 
-//version 2:
-
+// version 2
+// TrieNode 自调
 class TrieNode {
     public TrieNode[] children;
     public String word;
@@ -118,6 +118,7 @@ class TrieNode {
         children[pos].insert(word, index + 1);
     }
 }
+
 public class Solution {
     /**
      * @param board: A list of lists of character
@@ -180,5 +181,86 @@ public class Solution {
         }
         
         return board[x][y] != 0;
+    }
+}
+
+// version 3
+// HashMap 存 Prefix
+public class Solution {
+    public static int[] dx = {0, 1, -1, 0};
+    public static int[] dy = {1, 0, 0, -1};
+    /**
+     * @param board: A list of lists of character
+     * @param words: A list of string
+     * @return: A list of string
+     */
+    public List<String> wordSearchII(char[][] board, List<String> words) {
+        if (board == null || board.length == 0) {
+            return new ArrayList<>();
+        }
+        if (board[0] == null || board[0].length == 0) {
+            return new ArrayList<>();
+        }
+        
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        Map<String, Boolean> prefixIsWord = getPrefixSet(words);
+        Set<String> wordSet = new HashSet<>();
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                dfs(board, visited, i, j, "", prefixIsWord, wordSet);
+            }
+        }
+        
+        return new ArrayList<String>(wordSet);
+    }
+    
+    private Map<String, Boolean> getPrefixSet(List<String> words) {
+        Map<String, Boolean> prefixIsWord = new HashMap<>();
+        for (String word : words) {
+            for (int i = 0; i < word.length() - 1; i++) {
+                String prefix = word.substring(0, i + 1);
+                if (!prefixIsWord.containsKey(prefix)) {
+                    prefixIsWord.put(prefix, false);
+                }
+            }
+            prefixIsWord.put(word, true);
+        }
+        
+        return prefixIsWord;
+    }
+    
+    private void dfs(char[][] board,
+                     boolean[][] visited,
+                     int x,
+                     int y,
+                     String preWord,
+                     Map<String, Boolean> prefixIsWord,
+                     Set<String> wordSet) {
+        String word = preWord + board[x][y];
+        if (!prefixIsWord.containsKey(word)) {
+            return;
+        }
+        
+        if (prefixIsWord.get(word)) {
+            wordSet.add(word);
+        }
+        
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int adjX = x + dx[i];
+            int adjY = y + dy[i];
+            
+            if (!inside(board, adjX, adjY) || visited[adjX][adjY]) {
+                continue;
+            }
+            
+            dfs(board, visited, adjX, adjY, word, prefixIsWord, wordSet);
+        }
+        visited[x][y] = false;
+    }
+    
+    private boolean inside(char[][] board, int x, int y) {
+        return x >= 0 && x < board.length && y >= 0 && y < board[0].length;
     }
 }
