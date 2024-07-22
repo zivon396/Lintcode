af49f39e86b35c9ea5d9914566add1bcf657132f
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-22 16:41:49 -0700

    Update 51_Previous permutation.java

--- DFS/51_Previous permutation.java
+++ DFS/51_Previous permutation.java
@@ -37,3 +37,44 @@ public class Solution {
 // 3 4 1 2 5 =>
 // 3 4 5 2 1 =>
 // 3 2 5 4 1
+
+// 和 52 一样, 先 swap 再 reverse 也行
+class Solution {
+    public List<Integer> previousPermuation(List<Integer> nums) {
+        int i = nums.size() - 2;
+        while (i >= 0 && nums.get(i) <= nums.get(i + 1)) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.size() - 1;
+            while (j >= 0 && nums.get(i) <= nums.get(j)) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+        return nums;
+    }
+
+    public void swap(List<Integer> nums, int i, int j) {
+        Integer tmp = nums.get(i);
+		nums.set(i, nums.get(j));
+		nums.set(j, tmp);
+    }
+
+    public void reverse(List<Integer> nums, int start) {
+        int left = start, right = nums.size() - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
+// 3 4 1 2 5 =>
+// 3 4 5 2 1 =>
+// 3 2 5 4 1
+
+// 3 4 1 2 5 =>
+// 3 2 1 4 5 =>
+// 3 2 5 4 1
