ca36d0e1ca428087ce05a9b57f58664024e102ed
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-22 16:02:21 -0700

    Create 52_Next permutation.java

---
+++ DFS/52_Next permutation.java
@@ -0,0 +1,34 @@
+// 从后往前, 找到第一个下降的数, swap, reverse
+class Solution {
+    public int[] nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+        return nums;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
+// 3 5 4 2 1
