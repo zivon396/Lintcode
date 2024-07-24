80af3299fff277da6ff3b727f5fd7850437ce204
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-24 15:14:58 -0700

    Update 107_Word break.java

--- DFS/107_Word break.java
+++ DFS/107_Word break.java
@@ -1,3 +1,45 @@
+// DP
+// dp[i] => 前 i 个字符能否拆分成若干个单词
+public class Solution {
+    /*
+     * @param s: A string
+     * @param dict: A dictionary of words dict
+     * @return: A boolean
+     */
+    public boolean wordBreak(String s, Set<String> dict) {
+        if (s == null) {
+            return true;
+        }
+        
+        int maxLength = 0;
+        for (String word : dict) {
+            maxLength = Math.max(maxLength, word.length());
+        }
+      
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int l = 1; l <= maxLength; l++) {
+                if (i < l) {
+                    break;
+                }
+                if (!dp[i - l]) {
+                    continue;
+                }
+                String word = s.substring(i - l, i);
+                if (dict.contains(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+}
+
 // 记忆化搜索 stack overflow
 public class Solution {
     /**
