e3b6613313ce0f4da6eaf67e8cb5d72ece1d33a9
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-25 18:15:10 -0700

    Update 448_Inorder successor in BST.java

--- Binary tree/448_Inorder successor in BST.java
+++ Binary tree/448_Inorder successor in BST.java
@@ -36,3 +36,27 @@ public class Solution {
 }
 
 // 迭代 Traverse version
+public class Solution {
+    /**
+     * @param root: the given BST
+     * @param p: the given node
+     * @return: the in-order successor of the given node in the BST
+     */
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // write your code here
+        TreeNode succ = null;
+        while (root != null) {
+            if (p.val < root.val) {
+                succ = root;
+                root = root.left;
+            }
+            else {
+                root = root.right;
+            }
+        }
+        
+        return succ;        
+    }
+}
+
+// Stack version
