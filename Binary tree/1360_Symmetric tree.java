532de5373e600b97a040192cb7d1353a530ebf8c
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-26 16:37:19 -0700

    Update 1360_Symmetric tree.java

--- Binary tree/1360_Symmetric tree.java
+++ Binary tree/1360_Symmetric tree.java
@@ -32,3 +32,50 @@ public class Solution {
 }
 
 // BFS
+public class Solution {
+    /**
+     * @param root: root of the given tree
+     * @return: whether it is a mirror of itself 
+     */
+    public boolean isSymmetric(TreeNode root) {
+        // Write your code here
+        if (root == null) {
+            return true;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++){
+                TreeNode curt = queue.poll();
+                if (curt != null){
+                    queue.offer(curt.left);
+                    queue.offer(curt.right);
+                    level.add(curt.val);
+                }
+                else {
+                    level.add(-1);
+                }
+            }
+
+            if (!isValid(level)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid (List<Integer> list){
+        for (int i = 0; i < list.size() / 2; i++){
+            if (list.get(i) != list.get(list.size() - i - 1)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
