ad4a5a2c994af1ab55641205248b4b92c67df49b
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-25 17:22:51 -0700

    Update 86_Binary search tree iterator.java

--- Binary tree/86_Binary search tree iterator.java
+++ Binary tree/86_Binary search tree iterator.java
@@ -1,6 +1,7 @@
 // 最简洁版本
 // 访问所有节点用时 O(n), 均摊下来每个节点就是 O(1)
 // 不必再回溯, 因为一直在 pop, 可以保证每次 pop 的一定是上一个 pop 节点回溯路径中第一个右拐的点
+// 注意 901 无法用这个方法, 因为 901 初始放进来的节点不是一直向左走到底的, 放进来的顺序和 pop 的逻辑有矛盾
 public class BSTIterator {
     private Stack<TreeNode> stack = new Stack<>();
     
