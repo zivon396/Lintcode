fd45fdd15cb89f02c10b0449b2449c0211c56c39
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-28 19:29:30 -0700

    Update 1311_Lowest common ancestor of a binary search tree.java

--- Binary tree/1311_Lowest common ancestor of a binary search tree.java
+++ Binary tree/1311_Lowest common ancestor of a binary search tree.java
@@ -1,3 +1,27 @@
+// 迭代
+public class Solution {
+    /**
+     * @param root: root of the tree
+     * @param p: the node p
+     * @param q: the node q
+     * @return: find the LCA of p and q
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // write your code here
+        while (p.val < root.val && q.val < root.val || p.val > root.val && q.val > root.val){
+            if (p.val < root.val && q.val < root.val){
+                root = root.left;
+            }
+            else {
+                root = root.right;
+            }
+        }
+
+        return root;
+    }
+}
+
+// 递归 traverse
 public class Solution {
     /**
      * @param root: root of the tree
