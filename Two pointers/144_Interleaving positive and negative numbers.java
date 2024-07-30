b5d20c4bde4d368439c356b2494dc36bb560def0
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-30 03:00:07 -0700

    Update 144_Interleaving positive and negative numbers.java

--- Two pointers/144_Interleaving positive and negative numbers.java
+++ Two pointers/144_Interleaving positive and negative numbers.java
@@ -47,3 +47,58 @@ public class Solution {
         return count;
     }
 }
+
+// 先 partition, 再交换正负数
+public class Solution {
+    public void rerange(int[] A) {
+        int pos = 0, neg = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > 0) {
+                pos++;
+            } else {
+                neg++;
+            }
+        }
+        
+        partition(A, pos > neg);
+        interleave(A, pos == neg);
+    }
+    
+    private void partition(int[] A, boolean startPositive) {
+        int flag = startPositive ? 1 : -1;
+        int left = 0, right = A.length - 1;
+        while (left <= right) {
+            while (left <= right && A[left] * flag > 0) {
+                left++;
+            }
+            while (left <= right && A[right] * flag < 0) {
+                right--;
+            }
+            
+            if (left <= right) {
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                
+                left++;
+                right--;
+            }
+        }
+    }
+    
+    private void interleave(int[] A, boolean has_same_length) {
+        int left = 1, right = A.length - 1;
+        if (has_same_length) {
+            right = A.length - 2;
+        }
+        
+        while (left < right) {
+            int temp = A[left];
+            A[left] = A[right];
+            A[right] = temp;
+            
+            left += 2;
+            right -= 2;
+        }
+    }
+}
