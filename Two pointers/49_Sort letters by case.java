bc0bb4d461c7d03acd3b123111df0392a23e830f
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-30 03:09:11 -0700

    Update 49_Sort letters by case.java

--- Two pointers/49_Sort letters by case.java
+++ Two pointers/49_Sort letters by case.java
@@ -29,3 +29,28 @@ public class Solution {
         }
     }
 }
+
+// 同向
+public class Solution {
+    /**
+     * @param chars: The letter array you should sort by Case
+     * @return: nothing
+     */
+    public void sortLetters(char[] chars) {
+        // write your code here
+        if (chars == null || chars.length == 0){
+            return;
+        }
+
+        int left = 0, right = 0;
+        while (right < chars.length){
+            if (Character.isLowerCase(chars[right])){
+                char tmp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = tmp;
+                left++;
+            }
+            right++;
+        }
+    }
+}
