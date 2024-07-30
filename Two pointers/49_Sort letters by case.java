1b814026245750c02fe9ddd276f6be46f6a8689a
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-30 03:07:51 -0700

    Create 49_Sort letters by case.java

---
+++ Two pointers/49_Sort letters by case.java
@@ -0,0 +1,31 @@
+// 相向
+public class Solution {
+    /**
+     * @param chars: The letter array you should sort by Case
+     * @return: nothing
+     */
+    public void sortLetters(char[] chars) {
+        // write your code here
+        if (chars == null || chars.length == 0){
+            return;
+        }
+
+        int left = 0, right = chars.length - 1;
+        while (left <= right){
+            while (left <= right && Character.isLowerCase(chars[left])){
+                left++;
+            }
+            while (left <= right && Character.isUpperCase(chars[right])){
+                right--;
+            }
+
+            if (left <= right){
+                char tmp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
