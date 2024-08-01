ed1b30b1922e28e8533ae680a94af29f4f02b2ba
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-31 23:57:38 -0700

    Update 601_Flatten 2D vector.java

--- Hash - Heap - Queue - Stack/601_Flatten 2D vector.java
+++ Hash - Heap - Queue - Stack/601_Flatten 2D vector.java
@@ -36,3 +36,56 @@ public class Vector2D implements Iterator<Integer> {
     @Override
     public void remove() {}
 }
+
+// 两个栈
+public class Vector2D implements Iterator<Integer> {
+    Stack<List<Integer>> stack = new Stack<>();
+    Stack<Integer> stackj;
+    
+    public void pushListListToStack(List<List<Integer>> vec2d) {
+        Stack<List<Integer>> temp = new Stack<>();
+        for (List<Integer> nested : vec2d) {
+            temp.push(nested);
+        }
+        
+        while (!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+    }
+    
+    public void pushListToStack(List<Integer> vec) {
+        Stack<Integer> temp = new Stack<>();
+        for (Integer nested : vec) {
+            temp.push(nested);
+        }
+        
+        while (!temp.isEmpty()) {
+            stackj.push(temp.pop());
+        }
+    }
+    
+    public Vector2D(List<List<Integer>> vec2d) {
+        pushListListToStack(vec2d);
+        // Initialize your data structure here
+        stackj = new Stack<>();
+    }
+
+    public Integer next() {
+        // Write your code here
+        if(!hasNext()) {
+            return null;
+        }
+        
+        return stackj.pop();
+    }
+
+    public boolean hasNext() { // 准备下一个元素
+        // Write your code here
+        while (stackj.isEmpty() && !stack.isEmpty())
+            pushListToStack(stack.pop());
+        
+        return !stackj.isEmpty();
+    }
+    
+    public void remove() {}
+}
