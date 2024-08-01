ef33eaf0713b4b9217820c82dc7e6087138b78ab
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-07-31 23:28:41 -0700

    Update 128_Hash function.java

--- Hash - Heap - Queue - Stack/128_Hash function.java
+++ Hash - Heap - Queue - Stack/128_Hash function.java
@@ -1,10 +1,16 @@
-class Solution {
-    public int hashCode(char[] key, int HASH_SIZE) {
+public class Solution {
+    /**
+     * @param key: A string you should hash
+     * @param HASH_SIZE: An integer
+     * @return: An integer
+     */
+    public int hashCode(String key, int HASH_SIZE) {
+        // write your code here
         long ans = 0;
-        for(int i = 0; i < key.length; i++) {
-            ans = (ans * 33 + (int)(key[i])) % HASH_SIZE; 
+        for(int i = 0; i < key.length(); i++) {
+            ans = (ans * 33 + (int)(key.charAt(i))) % HASH_SIZE; 
         }
-	
-	return (int)ans;
+
+        return (int)ans;
     }
-};
+}
