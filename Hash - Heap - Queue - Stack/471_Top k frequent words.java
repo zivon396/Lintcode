ce01bbb4d1e7cdcc9f38fad3d2bef7a543329414
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-01 00:55:52 -0700

    Update 471_Top k frequent words.java

--- Hash - Heap - Queue - Stack/471_Top k frequent words.java
+++ Hash - Heap - Queue - Stack/471_Top k frequent words.java
@@ -1,69 +1,51 @@
-// HashMap + heap
 class Pair {
-    String key;
-    int value;
-    
-    Pair(String key, int value) {
-        this.key = key;
-        this.value = value;
+    public String s;
+    public int freq;
+    public Pair (String s, int freq){
+        this.s = s;
+        this.freq = freq;
     }
 }
 
 public class Solution {
     /**
-     * @param words an array of string
-     * @param k an integer
-     * @return an array of string
+     * @param words: an array of string
+     * @param k: An integer
+     * @return: an array of string
      */
-     
-    private Comparator<Pair> pairComparator = new Comparator<Pair>() {
-        public int compare(Pair left, Pair right) {
-            if (left.value != right.value) {
-                return left.value - right.value;
+    private Comparator<Pair> pairCom = new Comparator<Pair>(){
+        public int compare (Pair left, Pair right){
+            if (left.freq == right.freq){
+                return right.s.compareTo(left.s);
             }
-            return right.key.compareTo(left.key);
+
+            return left.freq - right.freq;
         }
     };
-    
+
     public String[] topKFrequentWords(String[] words, int k) {
-        if (k == 0) {
-            return new String[0];
+        // write your code here
+        String[] res = new String[k];
+        if (k == 0){
+            return res;
         }
-        
-        HashMap<String, Integer> counter = new HashMap<>();
-        for (String word : words) {
-            if (counter.containsKey(word)) {
-                counter.put(word, counter.get(word) + 1);
-            } else {
-                counter.put(word, 1);
+        Map<String, Integer> hash = new HashMap<>();
+        Queue<Pair> heap = new PriorityQueue<>(k, pairCom);
+        for (String s: words){
+            hash.put(s, hash.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s: hash.keySet()){
+            heap.add(new Pair(s, hash.get(s)));
+            if (heap.size() > k){
+                heap.poll();
             }
         }
-        
-        PriorityQueue<Pair> Q = new PriorityQueue<Pair>(k, pairComparator);
-        for (String word : counter.keySet()) {
-            Pair peak = Q.peek();
-            Pair newPair = new Pair(word, counter.get(word));
-            if (Q.size() < k) {
-                Q.add(newPair);
-            } else if (pairComparator.compare(newPair, peak) > 0) {
-                Q.poll();
-                Q.add(newPair);
-            }
+
+        for (int i = k - 1; i >= 0; i--){
+            res[i] = heap.poll().s;
         }
-        
-        String[] result = new String[k];
-        int index = 0;
-        while (!Q.isEmpty()) {
-            result[index++] = Q.poll().key;
-        }
-        
-        // reverse
-        for (int i = 0; i < index / 2; i++) {
-            String temp = result[i];
-            result[i] = result[index - i - 1];
-            result[index - i - 1] = temp;
-        }
-        
-        return result;
-     }
+
+        return res;
+    }
 }
