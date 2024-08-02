45f094333be0a30d96ec927b9613a2416972b69f
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-01 23:42:30 -0700

    Create 772_Group anagrams.java

---
+++ Hash - Heap - Queue - Stack/772_Group anagrams.java
@@ -0,0 +1,19 @@
+public class Solution {
+    /**
+     * @param strs: the given array of strings
+     * @return: The anagrams which have been divided into groups
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // write your code here
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] sc = s.toCharArray();
+            Arrays.sort(sc);
+            String key = String.valueOf(sc);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
