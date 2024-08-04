025e07f4eadc549763b17a30e410c5311bba2f7d
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-03 22:46:20 -0700

    Create 817_Range sum query 2D - mutable.java

---
+++ Array - list/817_Range sum query 2D - mutable.java
@@ -0,0 +1,54 @@
+public class NumMatrix {
+    private int[][] arr, bit;
+    int n, m;
+    
+    /**
+     * @return: nothing
+     */
+    public NumMatrix(int[][] matrix) {
+        n = matrix.length;
+        m = matrix[0].length;
+        arr = new int[n][m];
+        bit = new int[n + 1][m + 1];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                update(i, j, matrix[i][j]);
+            }
+        }
+    }
+    
+    public void update(int row, int col, int val) {
+        int delta = val - arr[row][col];
+        arr[row][col] = val;
+        
+        for (int i = row + 1; i <= n; i = i + lowbit(i)) {
+            for (int j = col + 1; j <= m; j = j + lowbit(j)) {
+                bit[i][j] += delta;
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return (
+            prefixSum(row2, col2) -
+            prefixSum(row2, col1 - 1) -
+            prefixSum(row1 - 1, col2) +
+            prefixSum(row1 - 1, col1 - 1)
+        );
+    }
+    
+    private int prefixSum(int row, int col) {
+        int sum = 0;
+        for (int i = row + 1; i > 0; i = i - lowbit(i)) {
+            for (int j = col + 1; j > 0; j = j - lowbit(j)) {
+                sum += bit[i][j];
+            }
+        }
+        return sum;
+    }
+    
+    private int lowbit(int x) {
+        return x & (-x);
+    }
+}
