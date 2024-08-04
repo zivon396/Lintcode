d7038ced1f26a0a481a28021029a6abaf78160c7
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-03 23:16:12 -0700

    Create 548_Intersection of two arrays II.java

---
+++ Array - list/548_Intersection of two arrays II.java
@@ -0,0 +1,38 @@
+// Sort + merge
+public class Solution {
+    /**
+     * @param nums1: an integer array
+     * @param nums2: an integer array
+     * @return: an integer array
+     *          we will sort your return value in output
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        // write your code here
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int i = 0, j = 0;
+        int[] temp = new int[nums1.length];
+        int index = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                temp[index++] = nums1[i];
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        int[] result = new int[index];
+        for (int k = 0; k < index; k++) {
+            result[k] = temp[k];
+        }
+        
+        return result;
+    }
+}
+
+// HashMap
