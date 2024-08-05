3778c82c6caec8ef201822c8cff52ff6f80dc381
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-05 09:37:34 -0700

    Update 189_First missing positive.java

--- Array - list/189_First missing positive.java
+++ Array - list/189_First missing positive.java
@@ -1,3 +1,39 @@
+// 每次把当前的数换到它应该在的 index 上
+// 时间复杂度 O(2n)
+// 空间复杂度 O(1)
+public class Solution {
+    /**
+     * @param A: An array of integers
+     * @return: An integer
+     */
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != i + 1) {
+                int tmp = nums[i];
+                int index = nums[i] - 1;
+                if (nums[index] == tmp) {
+                    break;
+                }
+                nums[i] = nums[index];
+                nums[index] = tmp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }   
+        }
+
+        return n + 1;
+    }
+}
+
 // 时间复杂度 O(n)
 // 空间复杂度 O(n)
 public class Solution {
