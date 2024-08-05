aabce825b4ad99e6cfe3348a0f29ad6d9cc09e69
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-05 03:33:39 -0700

    Create 1095_Maximum swap.java

---
+++ Hash - Heap - Queue - Stack/Stack/1095_Maximum swap.java
@@ -0,0 +1,30 @@
+// 统计出每一位数字最后出现的位置, 然后从最高位开始, 对于每一位尝试寻找一个在它右边的最大的数, 若能找到, 直接交换即可
+public class Solution {
+    /**
+     * @param num: a non-negative intege
+     * @return: the maximum valued number
+     */
+    public int maximumSwap(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            buckets[digits[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            for (int k = 9; k > digits[i] - '0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
+            }
+        }
+
+        return num;
+    }
+}
+
+// Monotonic Stack
