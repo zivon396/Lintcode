5a9425eaca8a55281d58ee7e4eb30f086ea4038f
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-05 09:28:59 -0700

    Update 189_First missing positive.java

--- Array - list/189_First missing positive.java
+++ Array - list/189_First missing positive.java
@@ -29,3 +29,23 @@ public class Solution {
         return n + 1;
     }
 }
+
+// 时间复杂度 O(n²)
+public class Solution {
+    /**
+     * @param A: An array of integers
+     * @return: An integer
+     */
+    public int firstMissingPositive(int[] A) {
+        // i 是要找的最小正整数, 从 1 开始, 最大是数组长度 + 1
+        for (int i = 1; i <= A.length + 1; i++) {
+            for (int j = 0; j < A.length; j++) {
+                if (A[j] == i) break; // 找到了, 跳出内循环, 找下一个最小数
+                if (j == A.length - 1 && A[j] != i) // 找到最后一位也不是, 返回 i
+                    return i;
+            }
+        }
+
+        return 1; // 数组为空的情况
+    }
+}
