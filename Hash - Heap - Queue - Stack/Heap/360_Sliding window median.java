0e4ced1c3de84665c9c75dcea1c9e85313aa514f
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-07 06:20:35 -0700

    Update 360_Sliding window median.java

--- Hash - Heap - Queue - Stack/Heap/360_Sliding window median.java
+++ Hash - Heap - Queue - Stack/Heap/360_Sliding window median.java
@@ -11,6 +11,67 @@ public class Solution {
      * @param k: An integer
      * @return: The median of the element inside the window at each moving
      */
+    private int half;
+    private Queue<Integer> minHeap;
+    private Queue<Integer> maxHeap;
+    public Solution (){
+        this.minHeap = new PriorityQueue<>();
+        this.maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    }
+
+    public List<Integer> medianSlidingWindow(int[] nums, int k) {
+        // write your code here
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0){
+            return res;
+        }
+
+        half = (k + 1) / 2;
+        for (int i = 0; i < k - 1; i++){
+            addNum(nums[i]);
+        }
+
+        for (int i = k - 1; i < nums.length; i++){
+            addNum(nums[i]);
+            res.add(maxHeap.peek());
+            removeNum(nums[i - k + 1]);
+        }
+
+        return res;
+    }
+
+    private void addNum (int num){
+        if (maxHeap.size() < half){
+            maxHeap.add(num);
+        }
+        else {
+            minHeap.add(num);
+        }
+
+        if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()){
+            int tmp = maxHeap.poll();
+            maxHeap.add(minHeap.poll());
+            minHeap.add(tmp);
+        }
+    }
+
+    private void removeNum (int num){
+        if (maxHeap.contains(num)){
+            maxHeap.remove(num);
+        }
+        else {
+            minHeap.remove(num);
+        }
+    }
+}
+
+// 九章答案版本
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @param k: An integer
+     * @return: The median of the element inside the window at each moving
+     */
     private Comparator<Integer> heapCom = new Comparator<Integer>() {
         public int compare (Integer x, Integer y){
             return y - x;
@@ -78,54 +139,57 @@ public class Solution {
      *            : A list of integers.
      * @return: The median of the element inside the window at each moving.
      */
+    private int half;
+    private TreeSet<Node> minHeap;
+    private TreeSet<Node> maxHeap;
     public ArrayList<Integer> medianSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        TreeSet<Node> minheap = new TreeSet<Node>();
-        TreeSet<Node> maxheap = new TreeSet<Node>();
-        ArrayList<Integer> result = new ArrayList<Integer> ();
+        minHeap = new TreeSet<Node>();
+        maxHeap = new TreeSet<Node>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
         
         if (k == 0)
             return result;
         
-        int half = (k+1)/2;
-        for(int i=0; i<k-1; i++) {
-            add(minheap, maxheap, half, new Node(i, nums[i]));
+        half = (k + 1) / 2;
+        for(int i = 0; i < k - 1; i++) {
+            add(new Node(i, nums[i]));
         }
-        for(int i=k-1; i<n; i++) {
-            add(minheap, maxheap, half, new Node(i, nums[i]));
-            result.add(maxheap.last().val);
-            remove(minheap, maxheap, new Node(i-k+1, nums[i-k+1]));
+        for(int i = k - 1; i < n; i++) {
+            add(new Node(i, nums[i]));
+            result.add(maxHeap.last().val);
+            remove(new Node(i - k + 1, nums[i - k + 1]));
         }
         
         return result;
     }
     
-    void add(TreeSet<Node>minheap, TreeSet<Node> maxheap, int size, Node node) {
+    void add(Node node) {
         // add 的过程可以保证 median 一定是 maxHeap 的 peek
-        if (maxheap.size() < size) {
-            maxheap.add(node);
+        if (maxHeap.size() < half) {
+            maxHeap.add(node);
         }
         else {
-            minheap.add(node);
+            minHeap.add(node);
         }
-        if (maxheap.size() == size) { // 这个 if 其实可以省去
-            if (minheap.size() > 0 && minheap.first().val < maxheap.last().val) {
-                Node s = minheap.first();
-                Node b = maxheap.last();
-                minheap.remove(s);
-                maxheap.remove(b);
-                minheap.add(b);
-                maxheap.add(s);
+        if (maxHeap.size() == half) { // 这个 if 其实可以省去
+            if (minHeap.size() > 0 && minHeap.first().val < maxHeap.last().val) {
+                Node s = minHeap.first();
+                Node b = maxHeap.last();
+                minHeap.remove(s);
+                maxHeap.remove(b);
+                minHeap.add(b);
+                maxHeap.add(s);
             }
         }
     }
     
-    void remove(TreeSet<Node>minheap, TreeSet<Node> maxheap, Node node) {
-        if (maxheap.contains(node)) {
-            maxheap.remove(node);
+    void remove(Node node) {
+        if (maxHeap.contains(node)) {
+            maxHeap.remove(node);
         }
         else {
-            minheap.remove(node);
+            minHeap.remove(node);
         }
     }
 }
