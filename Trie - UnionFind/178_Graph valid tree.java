d9ca333555ded0ea5206017db3b9459acf48aad8
zivon396 <42732539+zivon396@users.noreply.github.com>
2024-08-09 05:52:17 -0700

    Update 178_Graph valid tree.java

--- Trie - UnionFind/178_Graph valid tree.java
+++ Trie - UnionFind/178_Graph valid tree.java
@@ -31,8 +31,8 @@ public class Solution {
         }
         
         void union (int x, int y){
-            int fa_x = father.get(x);
-            int fa_y = father.get(y);
+            int fa_x = compressed_find(x);
+            int fa_y = compressed_find(y);
             if (fa_x != fa_y){
                 father.put(fa_x, fa_y);
             }
@@ -56,21 +56,67 @@ public class Solution {
         
         return true;
     }
+}
 
-    // 这个也行
-    // public boolean validTree(int n, int[][] edges) {
-    //     // write your code here
-    //     UnionFind uf = new UnionFind(n);
-    //     int size = n;
+// version 2
+public class Solution {
+    /**
+     * @param n: An integer
+     * @param edges: a list of undirected edges
+     * @return: true if it's a valid tree, or false
+     */
+    class UnionFind {
+        HashMap<Integer, Integer> father = new HashMap<>();
+        int size;
+        UnionFind (int n){
+            for (int i = 0; i < n; i++){
+                father.put(i, i);
+            }
+            size = n;
+        }
+        
+        int compressed_find (int x){
+            int parent = father.get(x);
+            while (parent != father.get(parent)){
+                parent = father.get(parent);
+            }
+            
+            int fa = x;
+            while (fa != father.get(fa)){
+                int tmp = father.get(fa);
+                father.put(fa, parent);
+                fa = tmp;
+            }
+            
+            return parent;
+        }
+        
+        void union (int x, int y){
+            int fa_x = compressed_find(x);
+            int fa_y = compressed_find(y);
+            if (fa_x != fa_y){
+                father.put(fa_x, fa_y);
+                size--;
+            }
+        }
 
-    //     for (int[] edge: edges){
-    //         if (uf.compressed_find(edge[0]) == uf.compressed_find(edge[1])){
-    //             return false;
-    //         }
-    //         uf.union(edge[0], edge[1]);
-    //         size--;
-    //     }
-
-    //     return size == 1;
-    // }
+        int getSize (){
+            return size;
+        }
+    }
+    
+    public boolean validTree(int n, int[][] edges) {
+        // write your code here
+        if (edges.length != n - 1){
+            return false;
+        }
+        
+        UnionFind uf = new UnionFind(n);
+        
+        for (int i = 0; i < edges.length; i++){
+            uf.union(edges[i][0], edges[i][1]);
+        }
+        
+        return uf.getSize() == 1;
+    }
 }
